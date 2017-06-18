ca77e018a8ec33e5746c2f6d67e6f1d47de84bda
Yuya Kono <takohati0821@gmail.com>
2017-06-18 19:58:43 +0900

    真偽値、浮動小数点数、条件分岐、代入のASTを追加

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -3,6 +3,14 @@ module Syntax (Name, Type, Expr(..)) where
 {-
 (defn add:int (x:int y:int)
   (+ x y))
+(defn gcd:int (m:int n:int)
+  (if (= m 0)
+      n
+      (if (<= m n)
+          (gcd m (- n m))
+          (gcd n (- m n)))))
+(let ((a:int (read-int)) (b:int (read-int)))
+  (print-int (gcd a b)))
 -}
 
 type Name = String
@@ -16,6 +24,10 @@ data Expr = Nil
           | Call Name [Expr]
           | Var Name
           | Int Int
+          | Bool Bool
+          | Float Double
+          | If Expr Expr Expr
+          | Let [(Name, Type, Expr)] [Expr]
           deriving Show
 
 defnAdd :: Expr
@@ -25,5 +37,23 @@ defnAdd = Defn { name = "add"
                , body = [Call "+" [Var "x", Var "y"]]
                }
 
+defnGcd :: Expr
+defnGcd = Defn { name = "gcd"
+               , returnType = "int"
+               , params = [("m", "int"), ("n", "int")]
+               , body = [If (Call "=" [Var "m", Int 0])
+                         (Var "n")
+                         (If (Call "<=" [Var "m", Var "n"])
+                           (Call "gcd" [ Var "m"
+                                       , Call "-" [Var "n", Var "m"]])
+                           (Call "gcd" [ Var "n"
+                                       , Call "-" [Var "m", Var "n"]]))]
+               }
+
+printgcd :: Expr
+printgcd = Let [ ("a", "int", Call "read-int" [])
+               , ("b", "int", Call "read-int" [])
+               ] [Call "gcd" [Var "a", Var "b"]]
+
 sample :: [Expr]
-sample = [defnAdd, Call "add" [Int 1, Int 2]]
+sample = [defnAdd, Call "add" [Int 1, Int 2], defnGcd, printgcd]
