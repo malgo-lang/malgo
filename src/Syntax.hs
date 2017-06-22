5375843ac4046102daaa8c91119fac50aed08213
Yuya Kono <takohati0821@gmail.com>
2017-06-22 14:05:59 +0900

    抽象構文木の設計に悩み中. とりあえずTiger言語っぽくする?

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,26 +1,16 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Syntax (Name, Type, Func(..), Expr(..)) where
-
-{-
-TODO: より良いプリティプリンタ
-(define (add:int x:int y:int)
-  (+ x y))
-(define (gcd:int m:int n:int)
-  (if (= m 0)
-      n
-      (if (<= m n)
-          (gcd m (- n m))
-          (gcd n (- m n)))))
-(define answer:int 42)
-(let ((a:int (read-int)))
-  (print-int (gcd a ans)))
--}
 
 type Name = String
 type Type = String
 
+data TypedVar = (:-:) Name Type
+  deriving Show
+
 data Func = Func { name       :: Name
                  , returnType :: Type
-                 , params     :: [(Name, Type)]
+                 , params     :: [TypedVar]
                  , body       :: [Expr]
                  }
   deriving Show
@@ -28,43 +18,17 @@ data Func = Func { name       :: Name
 data Expr = Nil
           | Int Int
           | Bool Bool
-          -- | Float Double
           | Defn Func
-          | Def Name Type Expr
+          | Def TypedVar Expr
           | Call Name [Expr]
           | Var Name
           | If Expr Expr Expr
-          | Let (Name, Type, Expr) [Expr]
+          | Let TypedVar Expr [Expr]
   deriving Show
 
-defnAdd :: Expr
-defnAdd = Defn $ Func { name = "add"
-                      , returnType = "int"
-                      , params = [("x", "int"), ("y", "int")]
-                      , body = [Call "+" [Var "x", Var "y"]]
-                      }
-
-defnGcd :: Expr
-defnGcd = Defn $ Func
-  { name = "gcd"
-  , returnType = "int"
-  , params = [("m", "int"), ("n", "int")]
-  , body = [If (Call "=" [Var "m", Int 0])
-             (Var "n")
-             (If (Call "<=" [Var "m", Var "n"])
-               (Call "gcd" [ Var "m"
-                           , Call "-" [Var "n", Var "m"]])
-               (Call "gcd" [ Var "n"
-                           , Call "-" [Var "m", Var "n"]]))]
-  }
-
-printgcd :: Expr
-printgcd =
-  Let ("a", "int", Call "read-int" []) [
-  Let ("b", "int", Call "read-int" []) [
-      Call "gcd" [Var "a", Var "b"]
-      ]
-  ]
-
-sample :: [Expr]
-sample = [defnAdd, Call "add" [Int 1, Int 2], defnGcd, printgcd]
+defineAdd =
+  Defn $ Func { name = "add"
+              , returnType = "int"
+              , params = ["x" :-: "int", "y" :-: "int"]
+              , body = [Call "+" [Var "x", Var "y"]]
+              }
