f59d7484d43c826a62fea8b64b916dfe373a4d83
Yuya Kono <takohati0821@gmail.com>
2017-07-11 20:43:31 +0900

    ppx_deriving.showを試してみる

--- ocaml/src/id.ml
+++ ocaml/src/id.ml
@@ -2,5 +2,4 @@ type t = string
 
 let counter = ref 0
 let genid s =
-  incr counter;
-  Printf.sprintf "%s.%d" s !counter
+  incr counter
--- ocaml/src/main.ml
+++ ocaml/src/main.ml
@@ -1,5 +1,9 @@
+type point2d = float * float
+[@@deriving show]
+
 let main () =
   Printf.printf "Hello, world!\n";
-  Printf.printf "%s\n" (Id.genid "hoge")
+  Printf.printf "%s\n" (Id.genid "hoge");
+  print_string (show_point2d (2.0, 3.0))
 
 let () = main ()
