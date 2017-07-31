cad016a8a64395ed52c690be7b33547953b11eb8
Yuya Kono <takohati0821@gmail.com>
2017-07-31 22:41:46 +1000

    ListT, VectorTを追加. FunTを変更

--- haskell/src/Language/Malgo/Syntax.hs
+++ haskell/src/Language/Malgo/Syntax.hs
@@ -3,6 +3,7 @@
 module Language.Malgo.Syntax where
 
 import           Control.Monad.State (evalState)
+import Data.List (intercalate)
 
 type Name = String
 data Type = UnitT
@@ -10,20 +11,30 @@ data Type = UnitT
           | BoolT
           | FloatT
           | SymbolT
+          | ListT Type
+          | VectorT Type
+          | FunT Type Type
   deriving Eq
 
 instance Show Type where
-  show UnitT   = "()"
-  show IntT    = "Int"
-  show BoolT   = "Bool"
-  show FloatT  = "Float"
-  show SymbolT = "Symbol"
+  show UnitT             = "()"
+  show IntT              = "Int"
+  show BoolT             = "Bool"
+  show FloatT            = "Float"
+  show SymbolT           = "Symbol"
+  show (ListT t)         = "(" ++ show t ++ ")"
+  show (VectorT t)       = "[" ++ show t ++ "]"
+  show (FunT p ret)    = show p ++ " -> " ++ show ret
+  -- show (FunT params ret) = "(" ++ showParams params ++ ")" ++ " -> " ++ show ret
+  --   where showParams []     = ""
+  --         showParams (x:xs) = show x ++ " " ++ showParams xs
 
 data AST = Symbol Name
          | Int Int
          | Float Double
          | Bool Bool
          | Typed AST Type
+         | Vector [AST]
          | List [AST]
 
 instance Show AST where
@@ -33,8 +44,9 @@ instance Show AST where
   show (Bool True)   = "#t"
   show (Bool False)  = "#f"
   show (Typed a t)   = show a ++ ":" ++ show t
-  show (List ((Symbol "quote"):x:[])) = "'" ++ show x
-  show (List ((Symbol "quote"):xs)) = "'" ++ show (List xs)
+  show (Vector xs)   = "[" ++ unwords (map show xs) ++ "]"
+  show (List [Symbol "quote", x]) = "'" ++ show x
+  show (List (Symbol "quote":xs)) = "'" ++ show (List xs)
   show (List l)      = "(" ++ show_list l ++ ")"
     where show_list []     = ""
           show_list [x]    = show x
@@ -45,3 +57,9 @@ sample2 = Typed (List [ Symbol "if"
                       , List [Symbol "==", Symbol "ans", Int 42]
                       , List [Symbol "quote", Symbol "yes"]
                       , List [Symbol "quote", Symbol "no"]]) SymbolT
+
+sample3 = Typed (List [Symbol "def"
+                      , List [Typed (Symbol "f") IntT, Typed (Symbol "x") IntT]
+                      , List [Symbol "*", Symbol "x", Symbol "x"]]) SymbolT
+
+sample4 = Typed (Vector [Symbol "a", Symbol "b"]) (VectorT SymbolT)
