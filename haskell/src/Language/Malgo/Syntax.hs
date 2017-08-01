9bef4233bae0e5b0fc3cc761f698d7e35d2096e7
Yuya Kono <takohati0821@gmail.com>
2017-08-01 12:52:45 +1000

    データ型としてのリストをList, 構文木としてのリストをTreeに分割

--- haskell/src/Language/Malgo/Syntax.hs
+++ haskell/src/Language/Malgo/Syntax.hs
@@ -23,19 +23,15 @@ instance Show Type where
   show FloatT            = "Float"
   show SymbolT           = "Symbol"
   show (ListT t)         = "List (" ++ show t ++ ")"
-  show (VectorT t)       = "Vector (" ++ show t ++ ")"
   show (FunT p ret)    = show p ++ " -> " ++ show ret
-  -- show (FunT params ret) = "(" ++ showParams params ++ ")" ++ " -> " ++ show ret
-  --   where showParams []     = ""
-  --         showParams (x:xs) = show x ++ " " ++ showParams xs
 
 data AST = Symbol Name
          | Int Int
          | Float Double
          | Bool Bool
          | Typed AST Type
-         | Vector [AST]
          | List [AST]
+         | Tree [AST]
 
 instance Show AST where
   show (Symbol name) = name
@@ -44,22 +40,23 @@ instance Show AST where
   show (Bool True)   = "#t"
   show (Bool False)  = "#f"
   show (Typed a t)   = show a ++ ":" ++ show t
-  show (Vector xs)   = "[" ++ unwords (map show xs) ++ "]"
-  show (List [Symbol "quote", x]) = "'" ++ show x
-  show (List (Symbol "quote":xs)) = "'" ++ show (List xs)
-  show (List l)      = "(" ++ show_list l ++ ")"
-    where show_list []     = ""
-          show_list [x]    = show x
-          show_list (x:xs) = show x ++ " " ++ show_list xs
+  show (List xs)   = "[" ++ unwords (map show xs) ++ "]"
+  show (Tree xs) = "(" ++ unwords (map show xs) ++ ")"
+  -- show (List [Symbol "quote", x]) = "'" ++ show x
+  -- show (List (Symbol "quote":xs)) = "'" ++ show (List xs)
+  -- show (List l)      = "(" ++ show_list l ++ ")"
+  --   where show_list []     = ""
+  --         show_list [x]    = show x
+  --         show_list (x:xs) = show x ++ " " ++ show_list xs
 
-sample1 = List [Symbol "def", Typed (Symbol "ans") IntT, Int 42]
-sample2 = Typed (List [ Symbol "if"
-                      , List [Symbol "==", Symbol "ans", Int 42]
-                      , List [Symbol "quote", Symbol "yes"]
-                      , List [Symbol "quote", Symbol "no"]]) SymbolT
+sample1 = Tree [Symbol "def", Typed (Symbol "ans") IntT, Int 42]
+sample2 = Typed (Tree [ Symbol "if"
+                      , Tree [Symbol "==", Symbol "ans", Int 42]
+                      , Tree [Symbol "quote", Symbol "yes"]
+                      , Tree [Symbol "quote", Symbol "no"]]) SymbolT
 
-sample3 = Typed (List [Symbol "def"
-                      , List [Typed (Symbol "f") IntT, Typed (Symbol "x") IntT]
-                      , List [Symbol "*", Symbol "x", Symbol "x"]]) SymbolT
+sample3 = Typed (Tree [Symbol "def"
+                      , Tree [Typed (Symbol "f") IntT, Typed (Symbol "x") IntT]
+                      , Tree [Symbol "*", Symbol "x", Symbol "x"]]) SymbolT
 
-sample4 = Typed (Vector [Symbol "a", Symbol "b"]) (VectorT SymbolT)
+sample4 = Typed (List [Symbol "a", Symbol "b"]) (ListT SymbolT)
