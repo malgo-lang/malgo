2b33bd38e41896844f375fb320c02a9032277ccb
Yuya Kono <takohati0821@gmail.com>
2017-08-19 18:22:39 +1000

    色々変更

--- src/Language/Malgo/Parser.hs
+++ src/Language/Malgo/Parser.hs
@@ -29,20 +29,23 @@ stringLiteral = Tok.stringLiteral lexer
 charLiteral = Tok.charLiteral lexer
 
 parseUntyped = do
-  pos <- getPosition
-  try (fmap (Symbol pos) identifier)
-    <|> try (fmap (Float pos) float)
-    <|> try (fmap (Int pos) integer)
-    <|> try (reserved "#t" >> return (Bool pos True))
-    <|> try (reserved "#f" >> return (Bool pos False))
-    <|> try (fmap (Char pos) charLiteral)
-    <|> try (fmap (String pos) stringLiteral)
-    <|> try (char '\'' >> identifier >>= \s -> return (Tree [Symbol pos "quote", Symbol (incSourceColumn pos 1) s]))
-    <|> fmap (List pos) (brackets (many parseExpr))
+  try (fmap Symbol identifier)
+    <|> try (fmap Float float)
+    <|> try (fmap Int integer)
+    <|> try (reserved "#t" >> return (Bool True))
+    <|> try (reserved "#f" >> return (Bool False))
+    <|> try (fmap Char charLiteral)
+    <|> try (fmap String stringLiteral)
+    <|> try (char '\'' >> identifier >>= \s -> return (Tree [Symbol "quote", Symbol s]))
+    <|> fmap List (brackets (many parseExpr))
     <|> fmap Tree (parens (many parseExpr))
 
-parseTyped = parseUntyped >>=  \e -> reservedOp ":" >> getPosition >>= \pos -> parseUntyped >>= \t -> return (Typed pos e t)
+parseTyped = do
+  e <- parseUntyped
+  reservedOp ":"
+  t <- parseUntyped
+  return (Typed e t)
 
 parseExpr = try parseTyped <|> parseUntyped
 
-parse = Text.Parsec.parse parseExpr "test"
+parse = Text.Parsec.parse parseExpr ""
--- src/Language/Malgo/Tapl/Untyped.hs
+++ src/Language/Malgo/Tapl/Untyped.hs
@@ -1,1 +1,21 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Language.Malgo.Tapl.Untyped where
+
+import           Control.Monad.Identity
+import           Data.List              (elemIndex)
+import           Language.Malgo.Parser
+import           Language.Malgo.Syntax
+
+data Term = Var Name
+          | Abs Name Term
+          | App Term Term
+          deriving (Eq, Show)
+
+trans (Tree [Symbol "^", Symbol x, body]) = fmap (Abs x) (trans body)
+trans (Tree [x]) = trans x
+trans (Tree (x:rest)) = liftM2 App (trans x) (trans (Tree rest))
+trans (Symbol x) = return $ Var x
+
+parseLambda src = case parse src of
+  Right ast -> Right $ runIdentity (trans ast)
+  Left msg  -> Left msg
