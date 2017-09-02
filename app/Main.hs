1ae6f4df50e174975ad102cdc7e72394c478b687
Yuya Kono <takohati0821@gmail.com>
2017-09-02 20:55:55 +0900

    書き直す

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,7 @@
 module Main where
 
-import qualified Language.Malgo.Parser as Parser
-import qualified Language.Malgo.Syntax as Syntax
-import           System.Environment    (getArgs)
-import qualified Text.Parsec.String    as P
+import           System.Environment (getArgs)
+import qualified Text.Parsec.String as P
 
 main :: IO ()
-main = do
-  args <- getArgs
-  let file = head args
-  result <- P.parseFromFile Parser.parseExpr file
-  case result of
-    Left err  -> print err
-    Right ast -> print ast
+main = return ()
