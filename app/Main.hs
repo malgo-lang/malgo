c49ca0f068a6e087923ca1df244f930cc33d754f
Yuya Kono <takohati0821@gmail.com>
2017-09-23 18:28:11 +1000

    resolverをlts-9.5に

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import           System.Environment (getArgs)
+import qualified Language.Malgo.Parser as Parser
+import qualified Language.Malgo.Syntax as Syntax
+import           System.Environment    (getArgs)
+import qualified Text.Parsec.String    as P
 
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  let file = head args
+  result <- P.parseFromFile Parser.parseToplevel file
+  case result of
+    Left err  -> print err
+    Right ast -> print ast
