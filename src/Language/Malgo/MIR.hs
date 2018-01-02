2a9392b7cca978b847911f4ba5ae059f7cb566c5
Yuya Kono <takohati0821@gmail.com>
2018-01-02 12:11:43 +0900

    MIRを実装

---
+++ src/Language/Malgo/MIR.hs
@@ -0,0 +1,48 @@
+module Language.Malgo.MIR
+  ( Decl(..)
+  , Expr(..)
+  , Instr(..)
+  ) where
+
+import           Language.Malgo.HIR    (Op (..))
+import qualified Language.Malgo.HIR    as H
+import           Language.Malgo.Syntax (Type (..))
+import qualified Language.Malgo.Syntax as S
+import           Language.Malgo.Utils
+import           Text.PrettyPrint
+
+data Expr a = Int Integer
+            | Float Double
+            | Bool Bool
+            | Char Char
+            | String String
+            | Unit
+            | Call { _fn   :: a
+                   , _args :: [a]
+                   , _fv   :: [a]
+                   }
+            | If a (Expr a) (Expr a)
+            | BinOp Op a a
+            deriving (Show)
+
+data Instr a = a := (Expr a)
+             | Do (Expr a)
+             deriving Show
+
+data Decl a = FunDec { _name    :: a
+                     , _params  :: [a]
+                     , _capture :: [a]
+                     , _body    :: [Instr a]
+                     }
+            | StrDec { _name :: a
+                     , _val  :: String
+                     }
+            | ExDec { _name   :: a
+                    , _actual :: String
+                    }
+            deriving Show
+
+data Program a = Program { _toplevel :: [Decl a]
+                         , _main     :: [Instr a]
+                         }
+  deriving Show
