ddb3f68215a5d704880acfc9f23d389928acd361
Yuya Kono <takohati0821@gmail.com>
2018-05-20 00:28:35 +0900

    knormalをMonadMalgoを使ったものに一般化

--- src/Language/Malgo/KNormal.hs
+++ src/Language/Malgo/KNormal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE NoImplicitPrelude    #-}
 {-# LANGUAGE OverloadedStrings    #-}
@@ -15,87 +16,85 @@ import qualified Language.Malgo.Syntax  as S
 import           Language.Malgo.Type
 import           Language.Malgo.TypedID
 
-type KNormal a = Malgo UniqSupply a
-
-knormal :: S.Expr TypedID -> KNormal (Expr TypedID)
+knormal :: MonadMalgo UniqSupply m => S.Expr TypedID -> m (Expr TypedID)
 knormal e = transExpr e
 
-newTmp :: Name -> Type -> KNormal TypedID
-newTmp name typ = TypedID <$> (ID ("$" <> name) <$> newUniq) <*> pure typ
+newTmp :: MonadMalgo UniqSupply m => Name -> Type -> m TypedID
+newTmp name typ = TypedID <$> (ID ("$" <> name) <$> newUniq) <*> return typ
 
-newUnused :: KNormal TypedID
+newUnused :: MonadMalgo UniqSupply m => m TypedID
 newUnused = newTmp "_" "Unit"
 
-transOp :: S.Op -> Type -> KNormal Op
-transOp S.Add _  = pure Add
-transOp S.Sub _  = pure Sub
-transOp S.Mul _  = pure Mul
-transOp S.Div _  = pure Div
-transOp S.FAdd _ = pure FAdd
-transOp S.FSub _ = pure FSub
-transOp S.FMul _ = pure FMul
-transOp S.FDiv _ = pure FDiv
-transOp S.Mod _  = pure Mod
-transOp S.Eq ty  = pure $ Eq ty
-transOp S.Neq ty = pure $ Neq ty
-transOp S.Lt ty  = pure $ Lt ty
-transOp S.Gt ty  = pure $ Gt ty
-transOp S.Le ty  = pure $ Le ty
-transOp S.Ge ty  = pure $ Ge ty
-transOp S.And _  = pure And
-transOp S.Or _   = pure Or
+transOp :: MonadMalgo UniqSupply m => S.Op -> Type -> m Op
+transOp S.Add _  = return Add
+transOp S.Sub _  = return Sub
+transOp S.Mul _  = return Mul
+transOp S.Div _  = return Div
+transOp S.FAdd _ = return FAdd
+transOp S.FSub _ = return FSub
+transOp S.FMul _ = return FMul
+transOp S.FDiv _ = return FDiv
+transOp S.Mod _  = return Mod
+transOp S.Eq ty  = return $ Eq ty
+transOp S.Neq ty = return $ Neq ty
+transOp S.Lt ty  = return $ Lt ty
+transOp S.Gt ty  = return $ Gt ty
+transOp S.Le ty  = return $ Le ty
+transOp S.Ge ty  = return $ Ge ty
+transOp S.And _  = return And
+transOp S.Or _   = return Or
 
-insertLet :: S.Expr TypedID -> (TypedID -> KNormal (Expr TypedID)) -> KNormal (Expr TypedID)
+insertLet :: MonadMalgo UniqSupply m => S.Expr TypedID -> (TypedID -> m (Expr TypedID)) -> m (Expr TypedID)
 insertLet (S.Var _ x) k = k x
 insertLet v k = do
   x <- newTmp "k" (typeOf v)
   v' <- transExpr v
   e <- k x
-  pure (Let (ValDec x v') e)
+  return (Let (ValDec x v') e)
 
-bind :: [S.Expr TypedID] -> [TypedID] -> ([TypedID] -> KNormal (Expr TypedID)) -> KNormal (Expr TypedID)
+bind :: MonadMalgo UniqSupply m => [S.Expr TypedID] -> [TypedID] -> ([TypedID] -> m (Expr TypedID)) -> m (Expr TypedID)
 bind [] args k     = k (reverse args)
 bind (x:xs) args k = insertLet x (\x' -> bind xs (x' : args) k)
 
-transExpr :: S.Expr TypedID -> KNormal (Expr TypedID)
-transExpr (S.Var _ x) = pure (Var x)
-transExpr (S.Int _ x) = pure (Int x)
-transExpr (S.Float _ x) = pure (Float x)
-transExpr (S.Bool _ x) = pure (Bool x)
-transExpr (S.Char _ x) = pure (Char x)
-transExpr (S.String _ x) = pure (String x)
-transExpr (S.Unit _) = pure Unit
-transExpr (S.Tuple _ xs) = bind xs [] (pure . Tuple)
-transExpr (S.TupleAccess _ e i) = insertLet e (\e' -> pure $ TupleAccess e' i)
+transExpr :: MonadMalgo UniqSupply m => S.Expr TypedID -> m (Expr TypedID)
+transExpr (S.Var _ x) = return (Var x)
+transExpr (S.Int _ x) = return (Int x)
+transExpr (S.Float _ x) = return (Float x)
+transExpr (S.Bool _ x) = return (Bool x)
+transExpr (S.Char _ x) = return (Char x)
+transExpr (S.String _ x) = return (String x)
+transExpr (S.Unit _) = return Unit
+transExpr (S.Tuple _ xs) = bind xs [] (return . Tuple)
+transExpr (S.TupleAccess _ e i) = insertLet e (\e' -> return $ TupleAccess e' i)
 transExpr (S.Fn _ params body) = do
   body' <- transExpr body
   fn <- newFnId
-  pure (Let (FunDecs [FunDec fn (map fst params) body']) (Var fn))
+  return (Let (FunDecs [FunDec fn (map fst params) body']) (Var fn))
   where
     newFnId = newTmp "lambda" (FunTy (map snd params) (typeOf body))
-transExpr (S.Call _ fn args) = insertLet fn (\fn' -> bind args [] (pure . Call fn'))
+transExpr (S.Call _ fn args) = insertLet fn (\fn' -> bind args [] (return . Call fn'))
 transExpr (S.BinOp _ op e1 e2) = do
   op' <- transOp op (typeOf e1)
-  insertLet e1 (\x -> insertLet e2 (pure . BinOp op' x))
+  insertLet e1 (\x -> insertLet e2 (return . BinOp op' x))
 transExpr (S.If _ c t f) =
   insertLet
     c
     (\c' -> do
        t' <- transExpr t
        f' <- transExpr f
-       pure (If c' t' f'))
+       return (If c' t' f'))
 transExpr (S.Let info (S.ValDec _ name _ val:ds) body) = do
   val' <- transExpr val
   rest <- transExpr (S.Let info ds body)
-  pure (Let (ValDec name val') rest)
+  return (Let (ValDec name val') rest)
 transExpr (S.Let info (S.FunDec _ fn params _ fbody:ds) body) = do
   fbody' <- transExpr fbody
   rest <- transExpr (S.Let info ds body)
-  pure (Let (FunDecs [FunDec fn (map fst params) fbody']) rest)
+  return (Let (FunDecs [FunDec fn (map fst params) fbody']) rest)
 transExpr (S.Let info (S.ExDec _ name _ orig:ds) body) = Let (ExDec name orig) <$> transExpr (S.Let info ds body)
 transExpr (S.Let _ [] body) = transExpr body
 transExpr (S.Seq _ e1 e2) = do
   unused <- newUnused
   e1' <- transExpr e1
   e2' <- transExpr e2
-  pure $ Let (ValDec unused e1') e2'
+  return $ Let (ValDec unused e1') e2'
