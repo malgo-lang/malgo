1de4eb82490b9bb5ba98240aac54298873dc31b0
Yuya Kono <takohati0821@gmail.com>
2018-07-27 10:45:24 +0900

    TupleAccessのテストを追加

--- test/Language/Malgo/MiddleEnd/TransToIRSpec.hs
+++ test/Language/Malgo/MiddleEnd/TransToIRSpec.hs
@@ -15,10 +15,12 @@ spec =
   describe "trans" $ do
     t0 <- translate tuple
     it "Tuple" $ t0 `shouldBe` tuple'
+
+    t1 <- translate tupleAccess
+    it "TupleAccess" $ t1 `shouldBe` tupleAccess'
   where
-    translate e = runIO $ do
-      u <- newIORef 0
-      runMalgo (trans e) (UniqSupply u)
+    translate e = runIO $
+      runMalgo (trans e) . UniqSupply =<< newIORef 0
 
 x :: Info
 x = Info ("<dummy>", 0, 0)
@@ -30,3 +32,10 @@ tuple' :: I.Expr (ID I.MType)
 tuple' = I.Let (ID "$k" 0 (I.IntTy 32)) (I.Int 0)
          $ I.Let (ID "$k" 1 (I.IntTy 32)) (I.Int 1)
          $ I.Tuple[ID "$k" 0 (I.IntTy 32), ID "$k" 1 (I.IntTy 32)]
+
+tupleAccess :: S.Expr a
+tupleAccess = S.TupleAccess x tuple 0
+
+tupleAccess' :: I.Expr (ID I.MType)
+tupleAccess' = I.Let k tuple' $ I.Access k [0, 0]
+  where k = ID "$k" 2 (I.PointerTy (I.StructTy [I.IntTy 32, I.IntTy 32]))
