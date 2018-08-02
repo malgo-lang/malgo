30c2e155bdc9fb7ee0cf375e48a3b352d3af7b41
Yuya Kono <takohati0821@gmail.com>
2018-08-02 13:39:57 +0900

    Language.Malgo.FrontEnd.Lexerのテストを追加

--- test/Language/Malgo/FrontEnd/LexerSpec.hs
+++ test/Language/Malgo/FrontEnd/LexerSpec.hs
@@ -12,13 +12,17 @@ spec :: Spec
 spec =
   describe "lexer" $ do
     t0 <- lex () "<test>" ("let x = 42" :: Text)
-    it "let" $ t0 `shouldBe` Right [ Loc x LET
-                                   , Loc x (ID "x")
-                                   , Loc x EQUAL
-                                   , Loc x (INT 42)]
+    it "let" $ t0 `shouldBe` Right
+      [Loc x LET, Loc x (ID "x"), Loc x EQUAL, Loc x (INT 42)]
     t1 <- lex () "<test>" ("let rec f x = x + 42" :: Text)
     it "let rec" $ t1 `shouldBe` Right
       [Loc x LET, Loc x REC, Loc x (ID "f"), Loc x (ID "x"), Loc x EQUAL,
        Loc x (ID "x"), Loc x PLUS, Loc x (INT 42)]
+    t2 <- lex () "<test>" ("let rec f x = (x+.1.0).println" :: Text)
+    it "dot expr" $ t2 `shouldBe` Right
+      [Loc x LET, Loc x REC, Loc x (ID "f"), Loc x (ID "x"), Loc x EQUAL,
+       Loc x LPAREN, Loc x (ID "x"), Loc x PLUS_DOT, Loc x (FLOAT 1), Loc x RPAREN,
+       Loc x DOT, Loc x (ID "println")]
+
 x :: SrcSpan
 x = SrcSpan "<test>" 0 0 0 0
