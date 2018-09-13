c080c650cb781536adbde38bb9ad121e59cef543
Yuya Kono <takohati0821@gmail.com>
2018-09-13 19:03:30 +0900

    コード整理

--- src/Language/Malgo/Type.hs
+++ src/Language/Malgo/Type.hs
@@ -10,5 +10,9 @@ class HasType a where
   type TypeRep a :: *
   typeOf :: MonadReader (Env a) m => a -> m (TypeRep a)
 
-matchType :: (HasType a, HasType b, Eq (TypeRep a), MonadReader (Env a) m, Env a ~ Env b, TypeRep b ~ TypeRep a) => a -> b -> m Bool
+matchType
+  :: ( HasType a, HasType b
+     , Eq (TypeRep a), MonadReader (Env a) m
+     , Env a ~ Env b, TypeRep a ~ TypeRep b)
+  => a -> b -> m Bool
 matchType x y = (==) <$> typeOf x <*> typeOf y
