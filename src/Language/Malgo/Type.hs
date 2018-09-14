241f39e12f461942fe0e221afbf3ce4807ee3e1c
Yuya Kono <takohati0821@gmail.com>
2018-09-14 18:22:51 +0900

    Language.Malgo.Type.Type他を作成

--- src/Language/Malgo/Type.hs
+++ src/Language/Malgo/Type.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
 {-# LANGUAGE TypeFamilies          #-}
 module Language.Malgo.Type where
 
-import           Universum
+import           Universum hiding (Type)
 
 class HasType a where
   type Env a :: *
@@ -16,3 +17,28 @@ matchType
      , Env a ~ Env b, TypeRep a ~ TypeRep b)
   => a -> b -> m Bool
 matchType x y = (==) <$> typeOf x <*> typeOf y
+
+data TypeScheme a = Forall [a] (Type a)
+  deriving (Eq, Show)
+
+data Type a = TyApp (TyCon a) [Type a]
+            | TyVar a
+  deriving (Eq, Show)
+
+data TyCon a = IntC Integer
+             | Float32C
+             | Float64C
+             | TupleC Integer
+             | ArrayC
+             | ArrowC
+             | RecordC [Field a]
+             | TyFun [a] (Type a)
+             | Unique Integer (TyCon a)
+  deriving (Eq, Show)
+
+type Field a = (Text, Type a)
+
+instance HasType (Type a) where
+  type Env (Type a) = ()
+  type TypeRep (Type a) = Type a
+  typeOf = return
