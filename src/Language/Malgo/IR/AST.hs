5bfe9c32bfdfdb0ab4de5a9f8c2fdab08f119f7d
Yuya Kono <takohati0821@gmail.com>
2018-09-18 19:42:54 +0900

    コメントを追加

--- src/Language/Malgo/IR/AST.hs
+++ src/Language/Malgo/IR/AST.hs
@@ -8,8 +8,15 @@ data Expr a = Var SrcSpan a
             | Record SrcSpan [(a, Expr a)]
             | Variant SrcSpan a (Expr a) SType
             | Let (Bind a)
+            | Apply (Expr a) (Expr a)
             | Case SrcSpan (Expr a) [Clause a]
             | Fn SrcSpan [(a, Maybe SType)] (Expr a)
+
+{- # Function literal transformation
+   (\(x:a) (y:b) (z:c) -> e:d) : a -> b -> c -> d
+   => Fn SrcSpan [(x, a), (y, b), (z, c)] e
+   => Fn SrcSpan [(x, a)] (Fn SrcSpan [(y, b)] (Fn SrcSpan [(z, c)] e))
+-}
 
 data Literal = Int Integer
              | Float Double
@@ -19,14 +26,20 @@ data Literal = Int Integer
 data Bind a = NonRec SrcSpan a (Maybe SType) (Expr a)
             | Rec [(SrcSpan, a, Maybe SType, [a], Expr a)]
 
+{- # Rec transformation
+rec f x y : a -> b -> c = e
+=> Rec [(SrcSpan, f, a -> b -> c, [x, y], e)]
+=> Rec [(SrcSpan, f, a -> b -> c, [], Fn SrcSpan [(x, a)] (Fn SrcSpan [(y, b)] e))]
+-}
+
 data Clause a = VariantPat SrcSpan a a SType (Expr a)
               | BoolPat SrcSpan Bool (Expr a)
 
 -- | トップレベル宣言
 data Decl a = ScDef SrcSpan a [a] (Expr a) -- ^ 環境を持たない関数（定数）宣言
             | ScAnn SrcSpan a SType -- ^ 関数（定数）の型宣言
-            | TyDef SrcSpan SType [Text] SType -- ^ 型の別名定義
-            | NewTyDef SrcSpan SType [Text] SType -- ^ 新しい型の定義
+            | AliasDef SrcSpan SType [Text] SType -- ^ 型の別名定義
+            | TypeDef SrcSpan SType [Text] SType -- ^ 新しい型の定義
 
 
 -- | ソースコード上での型の表現
