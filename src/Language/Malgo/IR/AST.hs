5a2c36aac53b1196b7a9eb8bec4d34d26fbb9c09
Yuya Kono <takohati0821@gmail.com>
2018-09-19 15:46:58 +0900

    extendRecを修正

--- src/Language/Malgo/IR/AST.hs
+++ src/Language/Malgo/IR/AST.hs
@@ -1,24 +1,21 @@
-{-# LANGUAGE DeriveAnyClass        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies          #-}
 module Language.Malgo.IR.AST where
 
-import           Data.Outputable
 import           Language.Malgo.FrontEnd.Loc
-import           Language.Malgo.Type
 import           Universum
 
 data Expr a = Var SrcSpan a
             | Literal SrcSpan Literal
             | Record SrcSpan [(a, Expr a)]
             | Variant SrcSpan a (Expr a) SType
-            | Let [Bind a] (Expr a)
+            | Let (Bind a) (Expr a)
             | Apply (Expr a) (Expr a)
             | Case SrcSpan (Expr a) [Clause a]
             | Fn SrcSpan [(a, Maybe SType)] (Expr a)
-  deriving (Eq, Show, Generic, Outputable)
+  deriving (Eq, Show, Generic)
 
 {- # Function literal transformation
 (fun (x:a) (y:b) (z:c) -> e:d) : a -> b -> c -> d
@@ -34,56 +31,42 @@ data Literal = Int Integer
              | Float Double
              | Bool Bool
              | Char Char
-  deriving (Eq, Show, Generic, Outputable)
+  deriving (Eq, Show, Generic)
 
 data Bind a = NonRec SrcSpan a (Maybe SType) (Expr a)
-            | Rec SrcSpan a (Maybe SType) [a] (Expr a)
-  deriving (Eq, Show, Generic, Outputable)
-
-splitBinds :: [Bind a] -> [[Bind a]]
-splitBinds [] = []
-splitBinds (x@Rec{} : xs) =
-  (x:takeWhile isRec xs) : splitBinds (dropWhile isRec xs)
-splitBinds (x@NonRec{} : xs) =
-  (x:takeWhile (not . isRec) xs) : splitBinds (dropWhile (not . isRec) xs)
-
-isRec :: Bind a -> Bool
-isRec Rec{} = True
-isRec _     = False
+            | Rec [(SrcSpan, a, Maybe SType, [a], Expr a)]
+  deriving (Eq, Show, Generic)
 
 {- # Rec transformation
 rec f x y : a -> b -> c = e
-=> Rec SrcSpan f (a -> b -> c) [x, y] e
-=> Rec SrcSpan f (a -> b -> c) [] (Fn SrcSpan [(x, a)] (Fn SrcSpan [(y, b)] e))
+=> Rec [(SrcSpan, f, a -> b -> c, [x, y], e)]
+=> Rec [(SrcSpan, f, a -> b -> c, [], Fn SrcSpan [(x, Nothing)] (Fn SrcSpan [(y, Nothing)] e))]
 -}
 
-extendRec :: (HasType a, MonadReader (Env a) m, TypeRep a ~ SType) => Bind a -> m (Bind a)
-extendRec b@NonRec{} = return b
-extendRec b@(Rec _ _ _ [] _) = return b
-extendRec (Rec ss f ty xs e) =
-  Rec ss f ty [] <$> (buildFn <$> xs')
+extendRec :: Bind a -> Bind a
+extendRec (Rec bs) = Rec $ map extendRec' bs
   where
-    xs' = do
-      tys <- mapM typeOf xs
-      return $ reverse $ zip xs (map Just tys)
-    buildFn = foldr (\x -> Fn ss [x]) e
+    extendRec' b@(_, _, _, [], _) = b
+    extendRec' (ss, f, ty, xs, e) =
+      (ss, f, ty, [], foldr (\x -> Fn ss [x]) e $ reverse $ zip xs $ repeat Nothing)
+extendRec b = b
 
 data Clause a = VariantPat SrcSpan a a SType (Expr a)
               | BoolPat SrcSpan Bool (Expr a)
               | VarPat SrcSpan a (Expr a)
-  deriving (Eq, Show, Generic, Outputable)
+  deriving (Eq, Show, Generic)
 
 -- | トップレベル宣言
 data Decl a = ScDef SrcSpan a [a] (Expr a) -- ^ 環境を持たない関数（定数）宣言
             | ScAnn SrcSpan a SType -- ^ 関数（定数）の型宣言
             | AliasDef SrcSpan SType [Text] SType -- ^ 型の別名定義
             | TypeDef SrcSpan SType [Text] SType -- ^ 新しい型の定義
-  deriving (Eq, Show, Generic, Outputable)
+  deriving (Eq, Show, Generic)
 
 -- | ソースコード上での型の表現
 data SType = STyApp STyCon [SType]
            | STyVar Text
-  deriving (Eq, Show, Generic, Outputable)
+  deriving (Eq, Show, Generic)
 
 {- # SType vs Type
 型検査時にLanguage.Malgo.Type.Typeへ翻訳される．
@@ -93,4 +76,4 @@ Forallは型検査の過程で自動生成される．
 data STyCon = SimpleC Text
             | SRecordC [(Text, SType)]
             | SVariantC [(Text, SType)]
-  deriving (Eq, Show, Generic, Outputable)
+  deriving (Eq, Show, Generic)
