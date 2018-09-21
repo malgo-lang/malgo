a766eb5b570076fbadd5b3d546af215127cc6ced
Yuya Kono <takohati0821@gmail.com>
2018-09-21 19:38:45 +0900

    SrcInfoインスタンスを追加

--- src/Language/Malgo/IR/AST.hs
+++ src/Language/Malgo/IR/AST.hs
@@ -11,11 +11,21 @@ data Expr a = Var SrcSpan a
             | Literal SrcSpan Literal
             | Record SrcSpan [(a, Expr a)]
             | Variant SrcSpan a (Expr a) SType
-            | Let (Bind a) (Expr a)
+            | Let SrcSpan (Bind a) (Expr a)
             | Apply SrcSpan (Expr a) (Expr a)
             | Case SrcSpan (Expr a) [Clause a]
             | Fn SrcSpan [(a, Maybe SType)] (Expr a)
   deriving (Eq, Show, Generic)
+
+instance SrcInfo (Expr a) where
+  srcSpan (Var ss _)         = ss
+  srcSpan (Literal ss _)     = ss
+  srcSpan (Record ss _)      = ss
+  srcSpan (Variant ss _ _ _) = ss
+  srcSpan (Let ss _ _)       = ss
+  srcSpan (Apply ss _ _)     = ss
+  srcSpan (Case ss _ _)      = ss
+  srcSpan (Fn ss _ _)        = ss
 
 {- # Function literal transformation
 (fun (x:a) (y:b) (z:c) -> e:d) : a -> b -> c -> d
@@ -37,6 +47,11 @@ data Bind a = NonRec SrcSpan a (Maybe SType) (Expr a)
             | Rec [(SrcSpan, a, Maybe SType, [a], Expr a)]
   deriving (Eq, Show, Generic)
 
+instance SrcInfo (Bind a) where
+  srcSpan (NonRec ss _ _ _) = ss
+  srcSpan (Rec xs) =
+    foldl1 (curry srcSpan) $ map (view _1) xs
+
 {- # Rec transformation
 rec f x y : a -> b -> c = e
 => Rec [(SrcSpan, f, a -> b -> c, [x, y], e)]
@@ -56,12 +71,23 @@ data Clause a = VariantPat SrcSpan a a SType (Expr a)
               | VarPat SrcSpan a (Expr a)
   deriving (Eq, Show, Generic)
 
+instance SrcInfo (Clause a) where
+  srcSpan (VariantPat ss _ _ _ _) = ss
+  srcSpan (BoolPat ss _ _)        = ss
+  srcSpan (VarPat ss _ _)         = ss
+
 -- | トップレベル宣言
 data Decl a = ScDef SrcSpan a [a] (Expr a) -- ^ 環境を持たない関数（定数）宣言
             | ScAnn SrcSpan a SType -- ^ 関数（定数）の型宣言
             | AliasDef SrcSpan SType [Text] SType -- ^ 型の別名定義
             | TypeDef SrcSpan SType [Text] SType -- ^ 新しい型の定義
   deriving (Eq, Show, Generic)
+
+instance SrcInfo (Decl a) where
+  srcSpan (ScDef ss _ _ _)    = ss
+  srcSpan (ScAnn ss _ _)      = ss
+  srcSpan (AliasDef ss _ _ _) = ss
+  srcSpan (TypeDef ss _ _ _)  = ss
 
 -- | ソースコード上での型の表現
 data SType = STyApp STyCon [SType]
