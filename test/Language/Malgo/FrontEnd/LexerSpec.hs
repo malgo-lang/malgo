6ee00c37303bda89be64bf62f29774db5f28da7f
Yuya Kono <takohati0821@gmail.com>
2018-09-25 16:07:42 +0900

    fix test

--- test/Language/Malgo/FrontEnd/LexerSpec.hs
+++ test/Language/Malgo/FrontEnd/LexerSpec.hs
@@ -13,16 +13,20 @@ spec =
   describe "lexer" $ do
     t0 <- lex "<test>" ("let x = 42" :: Text)
     it "let" $ t0 `shouldBe` Right
-      [Loc x LET, Loc x (ID "x"), Loc x EQUAL, Loc x (INT 42)]
+      [ Loc (ss 1 1 1 5) LET
+      , Loc (ss 1 5 1 7) (ID "x")
+      , Loc (ss 1 7 1 9) EQUAL
+      , Loc (ss 1 9 1 11) (INT 42)]
     t1 <- lex "<test>" ("let rec f x = x + 42" :: Text)
     it "let rec" $ t1 `shouldBe` Right
-      [Loc x LET, Loc x REC, Loc x (ID "f"), Loc x (ID "x"), Loc x EQUAL,
-       Loc x (ID "x"), Loc x PLUS, Loc x (INT 42)]
-    t2 <- lex "<test>" ("let rec f x = (x+.1.0).println" :: Text)
-    it "dot expr" $ t2 `shouldBe` Right
-      [Loc x LET, Loc x REC, Loc x (ID "f"), Loc x (ID "x"), Loc x EQUAL,
-       Loc x LPAREN, Loc x (ID "x"), Loc x PLUS_DOT, Loc x (FLOAT 1), Loc x RPAREN,
-       Loc x DOT, Loc x (ID "println")]
+      [ Loc (ss 1 1 1 5) LET
+      , Loc (ss 1 5 1 9) REC
+      , Loc (ss 1 9 1 11) (ID "f")
+      , Loc (ss 1 11 1 13) (ID "x")
+      , Loc (ss 1 13 1 15) EQUAL
+      , Loc (ss 1 15 1 17) (ID "x")
+      , Loc (ss 1 17 1 19) PLUS
+      , Loc (ss 1 19 1 21) (INT 42)]
 
-x :: SrcSpan
-x = SrcSpan "<test>" 0 0 0 0
+ss :: Line -> Column -> Line -> Column -> SrcSpan
+ss = SrcSpan "<test>"
