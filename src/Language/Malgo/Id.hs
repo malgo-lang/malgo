33a46885d42e82f5d60823ab67c3041132fed110
Yuya Kono <takohati0821@gmail.com>
2018-10-14 18:42:15 +0900

    Pretty Idを実装

--- src/Language/Malgo/Id.hs
+++ src/Language/Malgo/Id.hs
@@ -1,6 +1,7 @@
 module Language.Malgo.Id where
 
 import           Language.Malgo.Monad
+import           Language.Malgo.Pretty
 import           Universum
 
 data Id = Id { _name :: Text, _uniq :: Int }
@@ -8,3 +9,6 @@ data Id = Id { _name :: Text, _uniq :: Int }
 
 newId :: MonadMalgo f => Text -> f Id
 newId name = Id name <$> newUniq
+
+instance Pretty Id where
+  pPrint (Id name uniq) = pPrint name <> "_" <> pPrint uniq
