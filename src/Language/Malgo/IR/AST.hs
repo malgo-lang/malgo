d5aa7fb457af1fa884e5451fbae0af0ae39433df
Yuya Kono <takohati0821@gmail.com>
2018-10-15 18:04:03 +0900

    ASTとTypeに対してSrcInfo, Prettyを実装

--- src/Language/Malgo/IR/AST.hs
+++ src/Language/Malgo/IR/AST.hs
@@ -19,8 +19,36 @@ data Expr a = Var SrcSpan a
             | Access SrcSpan (Expr a) Int
   deriving (Eq, Show)
 
-instance SrcInfo (Expr a)
-instance Pretty a => Pretty (Expr a)
+instance SrcInfo (Expr a) where
+  srcSpan (Var ss _)       = ss
+  srcSpan (Literal ss _)   = ss
+  srcSpan (BinOp ss _ _ _) = ss
+  srcSpan (If ss _ _ _)    = ss
+  srcSpan (Let ss _ _)     = ss
+  srcSpan (Apply ss _ _)   = ss
+  srcSpan (Tuple ss _)     = ss
+  srcSpan (Access ss _ _)  = ss
+
+instance Pretty a => Pretty (Expr a) where
+  pPrintPrec _ _ (Var _ x) = pPrint x
+  pPrintPrec _ _ (Literal _ x) = pPrint x
+  pPrintPrec l d (BinOp _ op x y) =
+    maybeParens (d > 5) $ pPrintPrec l 6 x <+> pPrint op <+> pPrintPrec l 6 y
+  pPrintPrec l d (If _ c t f) =
+    maybeParens (d > 3)
+    $ "if" <+> pPrintPrec l 4 c
+    $+$ "then" <+> pPrintPrec l 4 t
+    $+$ "else" <+> pPrintPrec l 4 f
+  pPrintPrec l d (Let _ bind expr) =
+    maybeParens (d > 1)
+    $ "let" <+> pPrintPrec l 0 bind
+    $+$ "in" <+> pPrintPrec l 0 expr
+  pPrintPrec l d (Apply _ x y) =
+    maybeParens (d > 10) $ pPrintPrec l 11 x <+> pPrintPrec l 11 y
+  pPrintPrec l _ (Tuple _ xs) =
+    parens $ sep $ punctuate "," $ map (pPrintPrec l 0) xs
+  pPrintPrec l d (Access _ x i) =
+    maybeParens (d > 10) $ pPrintPrec l 11 x <> "." <> pPrint i
 
 data Literal = Int Integer
              | Float Double
@@ -29,7 +57,13 @@ data Literal = Int Integer
              | String String
   deriving (Eq, Show)
 
-instance Pretty Literal
+instance Pretty Literal where
+  pPrint (Int i)      = pPrint i
+  pPrint (Float d)    = pPrint d
+  pPrint (Bool True)  = "true"
+  pPrint (Bool False) = "false"
+  pPrint (Char c)     = quotes $ pPrint c
+  pPrint (String s)   = doubleQuotes $ pPrint s
 
 data Op = Add | Sub | Mul | Div | Mod
         | FAdd | FSub | FMul | FDiv
@@ -37,14 +71,30 @@ data Op = Add | Sub | Mul | Div | Mod
         | And | Or
   deriving (Eq, Show)
 
-instance Pretty Op
+instance Pretty Op where
+  pPrint op = case op of
+    { Add -> "+"; Sub -> "-"; Mul -> "*"; Div -> "/"; Mod -> "%"
+    ; FAdd -> "+."; FSub -> "-."; FMul -> "*."; FDiv -> "/."
+    ; Eq -> "=="; Neq -> "/="; Lt -> "<"; Gt -> ">"; Le -> "<="; Ge -> ">="
+    ; And -> "&"; Or -> "|" }
 
 data Bind a = NonRec SrcSpan a (Maybe (Type a)) (Expr a)
             | Rec SrcSpan a [a] (Maybe (Type a)) (Expr a)
   deriving (Eq, Show)
 
-instance SrcInfo (Bind a)
-instance Pretty a => Pretty (Bind a)
+instance SrcInfo (Bind a) where
+  srcSpan (NonRec ss _ _ _) = ss
+  srcSpan (Rec ss _ _ _ _)  = ss
+
+instance Pretty a => Pretty (Bind a) where
+  pPrint (NonRec _ x Nothing e) = pPrint x <+> "=" <+> pPrint e
+  pPrint (NonRec _ x (Just t) e) = pPrint x <+> ":" <+> pPrint t <+> "=" <+> pPrint e
+  pPrint (Rec _ f xs Nothing e) =
+    "rec" <+> pPrint f <+> sep (map pPrint xs) <+> "="
+    $+$ nest 2 (pPrint e)
+  pPrint (Rec _ f xs (Just t) e) =
+    "rec" <+> pPrint f <+> sep (map pPrint xs) <+> ":" <+> pPrint t <+> "="
+    $+$ nest 2 (pPrint e)
 
 -- | トップレベル宣言
 data Decl a = ScDef SrcSpan a [a] (Expr a) -- ^ 環境を持たない関数（定数）宣言
@@ -52,5 +102,14 @@ data Decl a = ScDef SrcSpan a [a] (Expr a) -- ^ 環境を持たない関数（�
             | TypeDef SrcSpan a [a] (Type a) -- ^ 型の別名宣言
   deriving (Eq, Show, Generic)
 
-instance SrcInfo (Decl a)
-instance Pretty a => Pretty (Decl a)
+instance SrcInfo (Decl a) where
+  srcSpan (ScDef ss _ _ _)   = ss
+  srcSpan (ScAnn ss _ _)     = ss
+  srcSpan (TypeDef ss _ _ _) = ss
+
+instance Pretty a => Pretty (Decl a) where
+  pPrint (ScDef _ f xs e) =
+    pPrint f <+> sep (map pPrint xs) <+> "="
+    $+$ nest 2 (pPrint e)
+  pPrint (ScAnn _ x t) = pPrint x <+> ":" <+> pPrint t
+  pPrint (TypeDef _ n ps t) = "type" <+> pPrint n <+> sep (map pPrint ps) <+> "=" <+> pPrint t
--- src/Language/Malgo/Type.hs
+++ src/Language/Malgo/Type.hs
@@ -7,7 +7,8 @@ import           Universum             hiding (Type)
 data TypeScheme a = Forall [a] (Type a)
   deriving (Eq, Show)
 
-instance Pretty a => Pretty (TypeScheme a)
+instance Pretty a => Pretty (TypeScheme a) where
+  pPrint (Forall xs ty) = "forall" <+> sep (map pPrint xs) <> "." <+> pPrint ty
 
 newtype TyRef a = TyRef (IORef (Maybe (Type a)))
   deriving Eq
@@ -23,12 +24,25 @@ data Type a = TyApp TyCon [Type a]
             | TyMeta (TyRef a)
   deriving (Eq, Show)
 
-instance Pretty a => Pretty (Type a)
+instance Pretty a => Pretty (Type a) where
+  pPrintPrec l d (TyApp ArrowC [x, y]) =
+    maybeParens (d > 5) $ pPrintPrec l 6 x <+> "->" <+> pPrintPrec l 6 y
+  pPrintPrec _ _ (TyApp (TupleC n) xs)
+    | length xs == n = parens $ sep $ punctuate "," $ map pPrint xs
+  pPrintPrec l d (TyApp con args) =
+    maybeParens (d > 10) $ pPrint con <+> sep (map (pPrintPrec l 11) args)
+  pPrintPrec _ _ (TyVar x) = pPrint x
+  pPrintPrec _ _ (TyMeta x) = pPrint x
 
 data PrimC = IntC | DoubleC | CharC | BoolC | StringC
   deriving (Eq, Show)
 
-instance Pretty PrimC
+instance Pretty PrimC where
+  pPrint IntC = "Int"
+  pPrint DoubleC = "Double"
+  pPrint CharC = "Char"
+  pPrint BoolC = "Bool"
+  pPrint StringC = "String"
 
 data TyCon = TupleC Int
            | ArrowC
@@ -36,4 +50,8 @@ data TyCon = TupleC Int
            | PrimC PrimC
   deriving (Eq, Show)
 
-instance Pretty TyCon
+instance Pretty TyCon where
+  pPrint (TupleC n) = "(" <> text (replicate n ',') <> ")"
+  pPrint ArrowC = "(->)"
+  pPrint ArrayC = "Array"
+  pPrint (PrimC c) = pPrint c
