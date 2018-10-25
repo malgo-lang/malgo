80100482d3fccc63f140d515c30e6a5f449d4da7
Yuya Kono <takohati0821@gmail.com>
2018-10-25 18:59:10 +0900

    Outputable Idを追加

--- src/Language/Malgo/Id.hs
+++ src/Language/Malgo/Id.hs
@@ -1,14 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Language.Malgo.Id where
 
+import           Data.Outputable
 import           Language.Malgo.Monad
 import           Language.Malgo.Pretty
 import           Universum
 
 data Id = Id { _name :: Text, _uniq :: Int }
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, Generic)
 
 newId :: MonadMalgo f => Text -> f Id
 newId name = Id name <$> newUniq
+
+instance Outputable Id
 
 instance Pretty Id where
   pPrint (Id name uniq) = pPrint name <> "_" <> pPrint uniq
