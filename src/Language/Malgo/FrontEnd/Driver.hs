2715b5bba2d67247e6cb2975d0dc29874570f6ae
Yuya Kono <takohati0821@gmail.com>
2018-11-05 15:34:31 +0900

    コード整形

--- src/Language/Malgo/FrontEnd/Driver.hs
+++ src/Language/Malgo/FrontEnd/Driver.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Language.Malgo.FrontEnd.Driver where
 
-import Language.Malgo.IR.AST
-import Language.Malgo.FrontEnd.Lexer
-import Language.Malgo.FrontEnd.Parser
-import Language.Malgo.FrontEnd.Rename
-import Language.Malgo.FrontEnd.RnTcEnv
-import Language.Malgo.FrontEnd.TypeCheck
-import Universum hiding (Type)
+import           Language.Malgo.FrontEnd.Lexer
+import           Language.Malgo.FrontEnd.Parser
+import           Language.Malgo.FrontEnd.Rename
+import           Language.Malgo.FrontEnd.RnTcEnv
+import           Language.Malgo.FrontEnd.TypeCheck
+import           Language.Malgo.IR.AST
+import           Universum                         hiding (Type)
 
 frontend filename = do
   source <- readFile filename
