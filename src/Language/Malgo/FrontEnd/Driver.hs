ac8ea2f74bcf1f81bea841e2d204603700d2e07d
Yuya Kono <takohati0821@gmail.com>
2018-11-05 13:20:24 +0900

    FrontEndのDriverを作成

---
+++ src/Language/Malgo/FrontEnd/Driver.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Language.Malgo.FrontEnd.Driver where
+
+import Language.Malgo.IR.AST
+import Language.Malgo.FrontEnd.Lexer
+import Language.Malgo.FrontEnd.Parser
+import Language.Malgo.FrontEnd.Rename
+import Language.Malgo.FrontEnd.RnTcEnv
+import Language.Malgo.FrontEnd.TypeCheck
+import Universum hiding (Type)
+
+frontend filename = do
+  source <- readFile filename
+  Right toks <- lex filename source
+  let decs = parse toks
+  rnTcEnv <- makeRnTcEnv
+  usingStateT rnTcEnv $ do
+    decs' <- rename decs
+    typeCheck decs'
+    return decs'
