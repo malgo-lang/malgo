18fff552dfd35f1dc8decdb1edd9f664cd82e0d0
Yuya Kono <takohati0821@gmail.com>
2018-11-09 17:44:24 +0900

    いくつかファイルを追加

---
+++ src/Language/Malgo/MiddleEnd/KNormal.hs
@@ -0,0 +1,6 @@
+module Language.Malgo.MiddleEnd.KNormal where
+
+import           Language.Malgo.FrontEnd.RnTcEnv
+import           Language.Malgo.IR.AST
+import           Language.Malgo.Type
+import           Universum               hiding ( Type )
