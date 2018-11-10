6d12776827f1d844301642ec378fe90646bf7d35
Yuya Kono <takohati0821@gmail.com>
2018-11-10 12:57:49 +0900

    KNormalの骨組みを作成

--- src/Language/Malgo/MiddleEnd/KNormal.hs
+++ src/Language/Malgo/MiddleEnd/KNormal.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
 module Language.Malgo.MiddleEnd.KNormal where
 
 import           Language.Malgo.FrontEnd.RnTcEnv
+import           Language.Malgo.Id
 import           Language.Malgo.IR.AST
 import           Language.Malgo.Type
-import           Universum               hiding ( Type )
+import           Universum                       hiding (Type)
+
+knormal :: MonadState RnTcEnv m => [Decl Id] -> m [Decl Id]
+knormal = undefined
+
+checkKNormalized :: [Decl Id] -> Bool
+checkKNormalized = undefined
