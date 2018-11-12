1d1f0f9e148253f2c814c601e055bd35f1bab474
Yuya Kono <takohati0821@gmail.com>
2018-11-12 17:27:19 +0900

    型注釈を追加

--- src/Language/Malgo/FrontEnd/Driver.hs
+++ src/Language/Malgo/FrontEnd/Driver.hs
@@ -4,9 +4,12 @@ module Language.Malgo.FrontEnd.Driver where
 import           Language.Malgo.FrontEnd.Rename
 import           Language.Malgo.FrontEnd.RnTcEnv
 import           Language.Malgo.FrontEnd.TypeCheck
+import           Language.Malgo.Id
 import           Language.Malgo.IR.AST
+import           Language.Malgo.Monad
 import           Universum               hiding ( Type )
 
+frontend :: MonadMalgo m => [Decl Text] -> m ([Decl Id], RnTcEnv)
 frontend decs = do
   rnTcEnv <- makeRnTcEnv
   usingStateT rnTcEnv $ do
