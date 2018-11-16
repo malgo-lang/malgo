03613ecaa76da1ec7d444132f0c703f69a7f3fa1
Yuya Kono <takohati0821@gmail.com>
2018-11-16 19:41:57 +0900

    outputableを一旦合併

---
+++ src/Data/Outputable.hs
@@ -0,0 +1,10 @@
+module Data.Outputable
+  ( Outputable(..)
+  , viaPretty
+  , viaPrettyNum
+  , viaShow
+  , viaShowNum
+  ) where
+
+import Data.Outputable.Class
+import Data.Outputable.Instances
