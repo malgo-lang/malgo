82dfe4f2834ce4366533cd47428cc96c57ddff0b
Yuya Kono <takohati0821@gmail.com>
2018-11-19 18:12:34 +0900

    クロージャ変換を行うLanguage.Malgo.MiddleEnd.Closureを作成

--- src/Language/Malgo/MiddleEnd/Closure.hs
+++ src/Language/Malgo/MiddleEnd/Closure.hs
@@ -1,1 +1,47 @@
-module Language.Malgo.MiddleEnd.Closure where
+{-# LANGUAGE ConstraintKinds           #-}
+{-# LANGUAGE DeriveAnyClass            #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE NoImplicitPrelude         #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TemplateHaskell           #-}
+module Language.Malgo.MiddleEnd.Closure
+  ( trans
+  )
+where
+
+import           Control.Lens                    (makeLenses)
+import           Data.Outputable
+import           Language.Malgo.FrontEnd.RnTcEnv
+import           Language.Malgo.Id
+import qualified Language.Malgo.IR.AST           as AST
+import           Language.Malgo.IR.MIR
+import           Language.Malgo.Monad
+import           Language.Malgo.Type
+import           Universum                       hiding (Type)
+
+data ClsEnv = ClsEnv { _typeEnv :: Map Id TypeRep
+                     , _defs    :: [Def]
+                     } deriving (Show, Generic, Outputable)
+makeLenses ''ClsEnv
+
+data ClsInfo = ClsInfo { _rnTcEnv :: RnTcEnv
+                       , _knowns  :: [Id]
+                       } deriving (Show, Generic, Outputable)
+makeLenses ''ClsInfo
+
+type Trans m = (MonadReader ClsInfo m, MonadState ClsEnv m, MonadMalgo m)
+
+trans :: MonadMalgo m => RnTcEnv -> [(Id, [Id], AST.Expr Id)] -> m [Def]
+trans rte xs =
+  map (view defs)
+    $ usingReaderT (ClsInfo rte [])
+    $ executingStateT (ClsEnv mempty [])
+    $ mapM_ transDef xs
+
+transDef :: Trans m => (Id, [Id], AST.Expr Id) -> m ()
+transDef = undefined
+
+transType :: Trans m => TypeScheme Id -> m TypeRep
+transType = undefined
