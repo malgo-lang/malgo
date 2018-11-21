c5846ee0c16072fb671be0a5e4a872409d98b89b
Yuya Kono <takohati0821@gmail.com>
2018-11-21 18:41:37 +0900

    Utilsを追加

---
+++ src/Language/Malgo/Utils.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE ExplicitForAll    #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TypeOperators     #-}
+module Language.Malgo.Utils where
+
+import           Data.Array.IArray
+import           Language.Malgo.FrontEnd.Loc
+import           Universum
+
+-- file operations
+newtype File = File (Array Line Text)
+  deriving (Show, Eq)
+
+newFile :: Text -> File
+newFile file = File $ listArray (1, length xs) xs
+  where xs = lines file
+
+viewSpan :: SrcInfo s => s -> File -> Text
+viewSpan s (File f)
+  | sline == eline =
+    let line = f ! sline
+    in toText $ drop (scolumn - 1) $ take (ecolumn - 1) $ toString line
+  | otherwise =
+    let firstline = toText $ drop (scolumn - 1) $ toString $ f ! sline
+        endline = toText $ take (ecolumn - 1) $ toString $ f ! eline
+        middle = map (f !) [sline + 1 .. eline - 1]
+    in unlines $ [firstline] <> middle <> [endline]
+  where
+    sline = startLine s
+    scolumn = startColumn s
+    eline = endLine s
+    ecolumn = endColumn s
+
+-- natural transformation
+type NaturalTransformation f g = forall a. f a -> g a
+type f ~> g = NaturalTransformation f g
