1c6296a45b2e06929b7c033a18b882a5e1c6e7d3
Yuya Kono <takohati0821@gmail.com>
2018-11-25 17:52:11 +0900

    コード整形

--- src/Language/Malgo/MiddleEnd/TypeOf.hs
+++ src/Language/Malgo/MiddleEnd/TypeOf.hs
@@ -4,20 +4,16 @@
 {-# LANGUAGE OverloadedStrings     #-}
 module Language.Malgo.MiddleEnd.TypeOf where
 
-import qualified Data.Map.Strict               as Map
+import qualified Data.Map.Strict                   as Map
 import           Language.Malgo.FrontEnd.Loc
 import           Language.Malgo.FrontEnd.RnTcEnv
-import           Language.Malgo.FrontEnd.TypeCheck
-                                                ( TcLclEnv(..)
-                                                , generalize
-                                                , instantiate
-                                                , unify
-                                                )
+import           Language.Malgo.FrontEnd.TypeCheck (TcLclEnv (..), generalize,
+                                                    instantiate, unify)
 import           Language.Malgo.Id
 import           Language.Malgo.IR.AST
 import           Language.Malgo.Monad
 import           Language.Malgo.Type
-import           Universum               hiding ( Type )
+import           Universum                         hiding (Type)
 
 typeOf :: (MonadMalgo m, MonadState RnTcEnv m) => Expr Id -> m (TypeScheme Id)
 typeOf = usingReaderT (TcLclEnv []) . typeOf'
