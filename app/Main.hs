bfe0e0394af7003337dd526ed0ed3a2e10a7860b
Yuya Kono <takohati0821@gmail.com>
2019-11-25 21:20:52 +0900

    app/とtest/をsrc/の変更に対応

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 module Main where
 
 import qualified Data.Text.Lazy.IO     as T
@@ -7,14 +6,14 @@ import qualified Language.Malgo.Lexer  as Lexer
 import           Language.Malgo.Monad
 import qualified Language.Malgo.Parser as Parser
 import           LLVM.Pretty
-import           Universum
+import           Data.IORef
 
 main :: IO ()
 main = do
   opt <- parseOpt
 
   let file = srcName opt
-  tokens <- Lexer.lexing () (toString file) =<< readFile file
+  tokens <- Lexer.lexing () file =<< readFile file
   let parser = Parser.parseExpr
   let ast = case parser <$> tokens of
               Left x  -> error $ show x
