84d65e22a0fd3cc4eed49dcf05f9959f0840f6b7
Yuya Kono <takohati0821@gmail.com>
2019-12-01 13:54:18 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 # examplesがコンパイルできるかチェック
 mkdir ./tmp
-ls ./examples | grep mlg | xargs -I{} sh -c 'echo {}; malgo ./examples/{} -o ./tmp/{}.ll; clang -lgc ./examples/lib.c ./tmp/{}.ll; ./a.out' && rm ./tmp/*.ll
+ls ./examples | grep mlg | xargs -I{} sh -c 'echo {}; cabal exec malgo -- ./examples/{} -o ./tmp/{}.ll; clang -lgc ./examples/lib.c ./tmp/{}.ll; ./a.out' && rm ./tmp/*.ll
