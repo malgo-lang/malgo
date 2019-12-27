aa2d7b7268f48e904988bd1f3d98502f069b7c94
Yuya Kono <takohati0821@gmail.com>
2019-12-27 17:46:35 +0900

    add SType.hs

---
+++ src/Language/Malgo/TypeRep/SType.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE DeriveGeneric     #-}
+module Language.Malgo.TypeRep.SType where
+
+import           Language.Malgo.Prelude
+import           Language.Malgo.Pretty
+
+data SType = TyVar Text
+           | TyInt
+           | TyFloat
+           | TyBool
+           | TyChar
+           | TyString
+           | TyFun [SType] SType
+           | TyTuple [SType]
+           | TyArray SType
+           | TyForall [Text] SType
+  deriving (Eq, Show, Read, Generic)
+
+instance Pretty SType where
+  pPrint (TyVar x) = text $ toString x
+  pPrint TyInt = "Int"
+  pPrint TyFloat = "Float"
+  pPrint TyBool = "Bool"
+  pPrint TyChar = "Char"
+  pPrint TyString = "String"
+  pPrint (TyFun ps r) = parens (sep $ punctuate "," $ map pPrint ps) <+> "->" <+> pPrint r
+  pPrint (TyTuple ts) = braces (sep $ punctuate "," $ map pPrint ts)
+  pPrint (TyArray t) = brackets $ pPrint t
+  pPrint (TyForall ts t) = "forall" <+> sep (map (text . toString) ts) <> "." <+> pPrint t
