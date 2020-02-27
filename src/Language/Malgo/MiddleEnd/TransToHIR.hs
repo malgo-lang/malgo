134702cd32df564c9669328e3ee49d94f28d8a0f
Yuya Kono <takohati0821@gmail.com>
2020-02-27 08:52:15 +0000

    TransToHIRの関数名を改良

--- src/Language/Malgo/MiddleEnd/TransToHIR.hs
+++ src/Language/Malgo/MiddleEnd/TransToHIR.hs
@@ -24,67 +24,58 @@ data TransToHIR
 instance Pass TransToHIR (S.Expr (ID Type)) (Expr (ID Type)) where
   passName = "TransToHIR"
   isDump   = dumpKNormal
-  trans e = flattenExpr <$> transToHIR e
+  trans e = flattenExpr <$> toExpr e
 
 newTmp :: MonadUniq f => String -> a -> f (ID a)
 newTmp n t = newID t ("$" <> n)
 
-insertLet :: (MonadUniq f, MonadWriter (Endo (Expr (ID Type))) f) => S.Expr (ID Type) -> f (ID Type)
-insertLet (S.Var _ x) = pure x
-insertLet v           = do
-  v' <- transToHIR v
-  x  <- newTmp "k" (typeOf v')
-  tell $ Endo $ Let x v'
-  pure x
+toVar :: (MonadUniq f, MonadWriter (Endo (Expr (ID Type))) f) => S.Expr (ID Type) -> f (ID Type)
+toVar = def <=< toExpr
 
-insertLet' :: (MonadUniq f, MonadWriter (Endo (Expr (ID Type))) f) => Expr (ID Type) -> f (ID Type)
-insertLet' (Var x) = pure x
-insertLet' v       = do
+def :: (MonadUniq f, MonadWriter (Endo (Expr (ID Type))) f) => Expr (ID Type) -> f (ID Type)
+def (Var x) = pure x
+def v       = do
   x <- newTmp "k" (typeOf v)
   tell $ Endo $ Let x v
   pure x
 
-appInsert :: Functor f => WriterT (Endo b) f b -> f b
-appInsert m = uncurry (flip appEndo) <$> runWriterT m
+runDef :: Functor f => WriterT (Endo b) f b -> f b
+runDef m = uncurry (flip appEndo) <$> runWriterT m
 
-transToHIR :: MonadUniq m => S.Expr (ID Type) -> m (Expr (ID Type))
-transToHIR (S.Var    _ a        ) = pure $ Var a
-transToHIR (S.Int    _ x        ) = pure $ Lit $ Int x
-transToHIR (S.Float  _ x        ) = pure $ Lit $ Float x
-transToHIR (S.Bool   _ x        ) = pure $ Lit $ Bool x
-transToHIR (S.Char   _ x        ) = pure $ Lit $ Char x
-transToHIR (S.String _ x        ) = pure $ Lit $ String x
-transToHIR (S.Tuple  _ xs       ) = appInsert $ Tuple <$> mapM insertLet xs
-transToHIR (S.Array  i (x :| xs)) = appInsert $ do
-  arr <- insertLet $ S.MakeArray i x (S.Int i $ fromIntegral $ length (x : xs))
-  forM_ (zip [1 ..] xs)
-    $ \(i, v) -> insertLet' =<< ArrayWrite arr <$> insertLet' (Lit (Int i)) <*> insertLet v
+toExpr :: MonadUniq m => S.Expr (ID Type) -> m (Expr (ID Type))
+toExpr (S.Var    _ a        ) = pure $ Var a
+toExpr (S.Int    _ x        ) = pure $ Lit $ Int x
+toExpr (S.Float  _ x        ) = pure $ Lit $ Float x
+toExpr (S.Bool   _ x        ) = pure $ Lit $ Bool x
+toExpr (S.Char   _ x        ) = pure $ Lit $ Char x
+toExpr (S.String _ x        ) = pure $ Lit $ String x
+toExpr (S.Tuple  _ xs       ) = runDef $ Tuple <$> mapM toVar xs
+toExpr (S.Array  _ (x :| xs)) = runDef $ do
+  arr <- def =<< MakeArray <$> toVar x <*> def (Lit $ Int $ fromIntegral $ length (x : xs))
+  forM_ (zip [1 ..] xs) $ \(i, v) -> def =<< ArrayWrite arr <$> def (Lit (Int i)) <*> toVar v
   pure $ Var arr
-transToHIR (S.MakeArray _ init size) = appInsert $ MakeArray <$> insertLet init <*> insertLet size
-transToHIR (S.ArrayRead _ arr  ix  ) = appInsert $ ArrayRead <$> insertLet arr <*> insertLet ix
-transToHIR (S.ArrayWrite _ arr ix val) =
-  appInsert $ ArrayWrite <$> insertLet arr <*> insertLet ix <*> insertLet val
-transToHIR (S.Call _ f  xs) = appInsert $ Call <$> insertLet f <*> mapM insertLet xs
-transToHIR (S.Fn   _ ps e ) = do
-  e' <- transToHIR e
+toExpr (S.MakeArray _ init size  ) = runDef $ MakeArray <$> toVar init <*> toVar size
+toExpr (S.ArrayRead _ arr  ix    ) = runDef $ ArrayRead <$> toVar arr <*> toVar ix
+toExpr (S.ArrayWrite _ arr ix val) = runDef $ ArrayWrite <$> toVar arr <*> toVar ix <*> toVar val
+toExpr (S.Call _ f  xs           ) = runDef $ Call <$> toVar f <*> mapM toVar xs
+toExpr (S.Fn   _ ps e            ) = do
+  e' <- toExpr e
   fn <- newTmp "lambda" (TyApp FunC (typeOf e' : map (typeOf . fst) ps))
   pure $ LetRec [Def { name = fn, params = map fst ps, expr = e' }] (Var fn)
-transToHIR (S.Seq _ e1                   e2  ) = appInsert $ insertLet e1 >> transToHIR e2
-transToHIR (S.Let _ (S.ValDec _ n _ val) body) = Let n <$> transToHIR val <*> transToHIR body
-transToHIR (S.Let _ (S.FunDec fundecs) body) =
-  LetRec <$> mapM transFunDec fundecs <*> transToHIR body
+toExpr (S.Seq _ e1                   e2  ) = runDef $ toVar e1 >> toExpr e2
+toExpr (S.Let _ (S.ValDec _ n _ val) body) = Let n <$> toExpr val <*> toExpr body
+toExpr (S.Let _ (S.FunDec fundecs  ) body) = LetRec <$> mapM transFunDec fundecs <*> toExpr body
  where
   transFunDec (_, fn, params, _, fbody) = do
-    fbody' <- transToHIR fbody
+    fbody' <- toExpr fbody
     pure Def { name = fn, params = map fst params, expr = fbody' }
-transToHIR (S.Let _ (S.ExDec _ n _ orig) body) = case typeOf n of
+toExpr (S.Let _ (S.ExDec _ n _ orig) body) = case typeOf n of
   TyApp FunC (_ : ps) -> do
     params <- mapM (newTmp "x") ps
-    LetRec [Def { name = n, params = params, expr = Prim orig (typeOf n) params }]
-      <$> transToHIR body
+    LetRec [Def { name = n, params = params, expr = Prim orig (typeOf n) params }] <$> toExpr body
   _ -> error "external variable is not supported"
-transToHIR (S.If    _ c  t f) = appInsert $ If <$> insertLet c <*> transToHIR t <*> transToHIR f
-transToHIR (S.BinOp _ op x y) = appInsert $ BinOp op' <$> insertLet x <*> insertLet y
+toExpr (S.If    _ c  t f) = runDef $ If <$> toVar c <*> toExpr t <*> toExpr f
+toExpr (S.BinOp _ op x y) = runDef $ BinOp op' <$> toVar x <*> toVar y
  where
   op' = case op of
     S.Add  -> Add
@@ -104,8 +95,8 @@ transToHIR (S.BinOp _ op x y) = appInsert $ BinOp op' <$> insertLet x <*> insert
     S.Ge   -> Ge
     S.And  -> And
     S.Or   -> Or
-transToHIR (S.Match _ s cs) =
-  appInsert $ Match <$> insertLet s <*> mapM (\(p, e) -> crushPat p =<< transToHIR e) cs
+toExpr (S.Match _ s cs) =
+  runDef $ Match <$> toVar s <*> mapM (\(p, e) -> crushPat p =<< toExpr e) cs
 
 crushPat :: MonadUniq m => S.Pat (ID Type) -> Expr (ID Type) -> m (Pat (ID Type), Expr (ID Type))
 crushPat (S.VarP   x ) e = pure (VarP x, e)
