606cfd76861f64be5272e14d458eb3858fba1d7a
Yuya Kono <takohati0821@gmail.com>
2020-02-27 07:45:42 +0000

    依存ライブラリの整理

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,16 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TypeApplications  #-}
 module Main where
 
 import           Language.Malgo.Driver
 import           Language.Malgo.Monad
-import           LLVM.AST                       ( Module )
-import           LLVM.Context                   ( withContext )
-import           LLVM.Module                    ( moduleLLVMAssembly
-                                                , withModuleFromAST
-                                                )
+import           LLVM.Pretty (ppllvm)
 import           Relude
-
-import qualified Data.Text.IO                  as T
 
 main :: IO ()
 main = do
   opt <- parseOpt
 
-  ll  <- compile opt =<< T.readFile (srcName opt)
+  ll  <- compile opt =<< readFileText (srcName opt)
 
-  writeFileText (dstName opt) =<< ppllvm ll
-
-ppllvm :: LLVM.AST.Module -> IO Text
-ppllvm ast =
-  withContext $ \ctx -> decodeUtf8 @Text <$> withModuleFromAST ctx ast moduleLLVMAssembly
+  writeFileLText (dstName opt) $ ppllvm ll
