cfc2cb41093295595f030c93631e8299ad030384
Yuya Kono <takohati0821@gmail.com>
2020-03-16 16:33:48 +0900

    cabalでもビルドできるように

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,12 @@
 import           Distribution.Simple
+import           Distribution.Simple.Setup      ( BuildFlags )
+import           Distribution.Types.HookedBuildInfo
+                                                ( HookedBuildInfo
+                                                , emptyHookedBuildInfo
+                                                )
+import           Hpack
 
-main = defaultMain
+main = do
+  defaultMainWithHooks $ simpleUserHooks
+    { preBuild = \_ _ -> hpack Verbose defaultOptions >> return emptyHookedBuildInfo
+    }
