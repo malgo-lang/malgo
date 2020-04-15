7299f24b4c20e60aec90e64a769cbdbc13c316a8
Yuya Kono <takohati0821@gmail.com>
2020-04-16 00:27:25 +0900

    update gen_examples.sh and test.sh

--- test.sh
+++ test.sh
@@ -2,12 +2,5 @@
 
 # examplesがコンパイルできるかチェック
 mkdir ./tmp
-
-for file in `ls ./examples | grep mlg`; do
-  echo $file
-  if stack exec malgo -- "./examples/${file}" -o "./tmp/${file}.ll" && clang -lgc ./examples/lib.c "./tmp/${file}.ll" && rm "./tmp/${file}.ll" && ./a.out; then
-    echo "PASS"
-  else
-    exit 1
-  fi
-done
+cabal build
+ls ./examples | grep mlg | xargs -I{} sh -c 'echo {} && cabal exec malgo -- ./examples/{} -o ./tmp/{}.ll && clang -lgc ./examples/lib.c ./tmp/{}.ll && rm ./tmp/{}.ll && ./a.out'
