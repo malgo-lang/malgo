b4bde7a3436990c95f724014f6a46c0a8ed211ef
Yuya Kono <takohati0821@gmail.com>
2020-04-15 19:12:24 +0900

    HasCType Typeを実装

--- src/Language/Malgo/TypeRep/CType.hs
+++ src/Language/Malgo/TypeRep/CType.hs
@@ -3,9 +3,10 @@
 {-# LANGUAGE OverloadedStrings  #-}
 module Language.Malgo.TypeRep.CType where
 
+import           Data.Text                   (pack)
 import           Language.Malgo.Id
 import           Language.Malgo.Prelude
--- import qualified Language.Malgo.TypeRep.Type as T
+import qualified Language.Malgo.TypeRep.Type as T
 
 data CType = CType :-> CType
     | IntT
@@ -33,3 +34,16 @@ instance HasCType CType where
 
 instance HasCType a => HasCType (Id a) where
   cTypeOf x = cTypeOf $ x ^. idMeta
+
+instance HasCType T.Type where
+  cTypeOf (T.TyApp T.IntC []) = PackT [Con "Int" [IntT]]
+  cTypeOf (T.TyApp T.FloatC []) = PackT [Con "Float" [FloatT]]
+  cTypeOf (T.TyApp T.BoolC []) = PackT [Con "True" [], Con "False" []]
+  cTypeOf (T.TyApp T.CharC []) = PackT [Con "Char" [CharT]]
+  cTypeOf (T.TyApp T.StringC []) = PackT [Con "String" [StringT]]
+  cTypeOf (T.TyApp T.TupleC xs) = PackT [Con ("Tuple" <> pack (show $ length xs)) (map cTypeOf xs)]
+  cTypeOf (T.TyApp T.ArrayC [x]) = ArrayT (cTypeOf x)
+  cTypeOf (T.TyApp _ _) = bug Unreachable
+  cTypeOf (T.TyMeta _) = AnyT
+  cTypeOf (ps T.:-> r) = PackT [Con ("Tuple" <> pack (show $ length ps)) (map cTypeOf ps)] :-> cTypeOf r
+  cTypeOf T.Kind = AnyT
