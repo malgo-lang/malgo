ceb8a4fcb10f3f518349b1e25b4d99c89577d1dc
Yuya Kono <takohati0821@gmail.com>
2020-04-17 16:45:40 +0900

    並列実行した場合a.outの実行が失敗するときがある不具合を修正

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 # examplesがコンパイルできるかチェック
 mkdir ./tmp
 stack build
-ls ./examples | grep mlg | xargs -P8 -I{} sh -c 'echo {} && stack exec malgo -- ./examples/{} -o ./tmp/{}.ll && clang -lgc ./examples/lib.c ./tmp/{}.ll && ./a.out'
+ls ./examples | grep mlg | xargs -P8 -I{} sh -c 'echo {} && stack exec malgo -- ./examples/{} -o ./tmp/{}.ll && clang -lgc ./examples/lib.c ./tmp/{}.ll -o ./tmp/{}.out && ./tmp/{}.out'
 rm ./tmp/*.ll
-rm ./a.out
+rm ./tmp/*.out
