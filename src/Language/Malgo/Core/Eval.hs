9c27881cb64dde73810d679770384e3adcfe18d3
Yuya Kono <takohati0821@gmail.com>
2020-04-30 13:35:36 +0900

    Coreのインタプリタを実装

--- src/Language/Malgo/Core/Eval.hs
+++ src/Language/Malgo/Core/Eval.hs
@@ -1,59 +1,160 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Language.Malgo.Core.Eval where
 
-import qualified Data.Map as Map
+import qualified Data.Text as T
+import Data.Vector.Mutable (IOVector)
+import qualified Data.Vector.Mutable as V
+import Debug.Trace (traceM, trace)
 import Language.Malgo.IR.Core
 import Language.Malgo.Id
 import Language.Malgo.Monad
 import Language.Malgo.Prelude
+import Language.Malgo.Pretty
+import Language.Malgo.TypeRep.CType
+import Text.PrettyPrint.HughesPJ (parens, sep)
 
-data Cont a
-  = MatchC [(Text, [a], Exp a)] (a, Exp a)
-  | ApplyC [Atom a]
-  deriving stock (Show)
+type Name = Id CType
 
-data Obj a
-  = FunO [a] (Exp a)
-  | PapO a [Atom a]
-  | PackO Text Int [Atom a]
+type EvalM a = StateT Env MalgoM a
 
--- step :: (Eq a, Ord a, MonadUniq m) => (Exp a, [Cont a], Map a (Obj a)) -> m (Exp a, [Cont a], Map a (Obj a))
--- step (Let x v e, s, h) = do
---   v' <- eval v h
---   x' <- newId (idMeta x) (idName x)
---   pure (subst x x' e, s, h & at x' ?~ v')
--- step (Match (Atom (Var v)) as (x, d), s, h) =
---   case h ^. at v of
---     Just (PackO tag n vs) -> go tag vs as
---     Nothing -> pure (subst x v d, s, h)
---   where
---     go tag0 vs ((tag1, ps, e):xs)
---       | tag0 == tag1 = pure (go' ps vs e, s, h)
---     go' [] [] e = e
---     go' (Var x:xs) (Var y:ys) e = go' xs ys (subst)
+data Env = Env {varMap :: Map Name Value}
 
--- eval :: (Eq a, Ord a, MonadUniq m) => Exp a -> Map a (Obj a) -> m (Obj a)
--- eval (Atom (Var a)) = do
---   stack <- ask
---   heap <- get
---   case stack ^. at a of
---     (Just (Var a')) -> case heap ^. at a' of
---       Just
--- case (stack ^. at a, heap ^. at a) of
---   (Just (Var a'), _) -> pure v
---   (_, Just v) -> pure v
---   _           -> error "unbound variable"
--- eval (Atom (Lit _)) = error "unboxed value"
--- eval (Call (Known n) f xs) | n == length xs = do
---   heap <- get
---   case heap ^. at f of
---     Just (FunO ps e) -> local (Map.fromList (zip ps xs) <>) $ eval e
+data Value
+  = FunV ([Value] -> EvalM Value)
+  | PackV Con [Value]
+  | ArrayV (IOVector Value)
+  | UnboxedV Unboxed
 
-subst :: (Functor f, Eq a) => a -> a -> f a -> f a
-subst a b f = fmap (\x -> if a == x then b else x) f
+instance Pretty Value where
+  pPrint FunV {} = "<function>"
+  pPrint (PackV con xs) = parens $ pPrint con <+> sep (map pPrint xs)
+  pPrint (ArrayV _) = "<array>"
+  pPrint (UnboxedV x) = pPrint x
 
-subst' :: (Functor f, Eq a) => [a] -> [a] -> f a -> f a
-subst' as bs f = appEndo ?? f $ mconcat $ zipWith (\a b -> Endo $ subst a b) as bs
+runEval :: EvalM a -> MalgoM a
+runEval = evalStateT ?? (Env mempty)
+
+defVar :: Name -> Value -> EvalM ()
+defVar x v = modify (\e -> e {varMap = varMap e & at x ?~ v})
+
+loadDef :: Name -> Obj Name -> EvalM ()
+loadDef x o = do
+  o' <- evalObj o
+  defVar x o'
+
+lookupVar :: Name -> EvalM Value
+lookupVar x = gets $ fromJust . (view $ at x) . varMap
+
+evalObj :: Obj Name -> EvalM Value
+evalObj (Fun ps e) = do
+  pure $ FunV $ \ps' -> do
+    env <- get
+    zipWithM_ defVar ps ps'
+    e' <- evalExp e
+    put env
+    pure e'
+evalObj (Pack con xs) = do
+  xs' <- traverse evalAtom xs
+  pure $ PackV con xs'
+evalObj (Array a n) = do
+  a' <- evalAtom a
+  n' <- evalAtom n
+  case n' of
+    UnboxedV (Int n'') -> do
+      arr <- liftIO $ V.replicate (fromIntegral n'') a'
+      pure $ ArrayV arr
+    _ -> bug Unreachable
+
+evalAtom :: Atom Name -> EvalM Value
+evalAtom (Var x) = lookupVar x
+evalAtom (Unboxed x) = pure $ UnboxedV x
+
+evalExp :: Exp Name -> EvalM Value
+evalExp (Atom x) = evalAtom x
+evalExp (Call f xs) = do
+  FunV f' <- lookupVar f
+  xs' <- traverse evalAtom xs
+  f' xs'
+evalExp (PrimCall prim _ xs) = do
+  prim' <- lookupPrim prim
+  xs' <- traverse evalAtom xs
+  prim' xs'
+evalExp (ArrayRead a i) = do
+  ArrayV vec <- evalAtom a
+  UnboxedV (Int idx) <- evalAtom i
+  liftIO $ V.read vec $ fromIntegral idx
+evalExp (ArrayWrite a i v) = do
+  ArrayV vec <- evalAtom a
+  UnboxedV (Int idx) <- evalAtom i
+  v' <- evalAtom v
+  liftIO $ V.write vec (fromIntegral idx) v'
+  pure unit
+evalExp (Let xs e) = do
+  traverse_ (uncurry loadDef) xs
+  evalExp e
+evalExp (Match e cs) = do
+  e' <- evalExp e
+  evalMatch e' cs
+
+evalMatch :: Value -> NonEmpty (Case Name) -> EvalM Value
+evalMatch (PackV con1 xs) (Unpack con2 ys e :| _)
+  | con1 == con2 = do
+    zipWithM_ defVar ys xs
+    evalExp e
+evalMatch v (Unpack {} :| (c : cs)) = evalMatch v (c :| cs)
+evalMatch v (Bind x e :| _) = do
+  defVar x v
+  evalExp e
+evalMatch v cs = do
+  traceM (show $ pPrint v)
+  traceM (show $ fmap pPrint cs)
+  undefined
+
+lookupPrim :: Text -> EvalM ([Value] -> EvalM Value)
+lookupPrim "+" = pure $ \case
+  [UnboxedV (Int x'), UnboxedV (Int y')] -> pure $ UnboxedV (Int $ x' + y')
+  _ -> bug Unreachable
+lookupPrim "-" = pure $ \case
+  [UnboxedV (Int x'), UnboxedV (Int y')] -> pure $ UnboxedV (Int $ x' - y')
+  _ -> bug Unreachable
+lookupPrim "<=" = pure $ \case
+  [x, y] ->
+    let order = compareValue x y
+     in pure $ boolToValue $ order == LT || order == EQ
+  _ -> bug Unreachable
+lookupPrim "print_int" = pure $ \case
+  [PackV (Con "Tuple1" [PackT [Con "Int" [IntT]]]) [PackV (Con "Int" [IntT]) [UnboxedV (Int x)]]] -> do
+    liftIO $ putStr $ show x
+    pure unit
+  _ -> bug Unreachable
+lookupPrim "newline" = pure $ \case
+  [PackV (Con "Tuple0" []) []] -> do
+    liftIO $ putStrLn ""
+    pure unit
+  xs -> trace (show $ pPrint xs) $ pure unit
+lookupPrim prim = error $ T.unpack $ "undefined primitive: " <> prim
+
+compareValue :: Value -> Value -> Ordering
+compareValue (UnboxedV x) (UnboxedV y) = compare x y
+compareValue (PackV con1 xs) (PackV con2 ys)
+  | con1 == con2 = go xs ys
+  where
+    go [] [] = EQ
+    go (a : as) (b : bs) = case compareValue a b of
+      EQ -> go as bs
+      order -> order
+    go _ _ = bug Unreachable
+compareValue _ _ = bug Unreachable
+
+boolToValue :: Bool -> Value
+boolToValue True = PackV (Con "True" []) []
+boolToValue False = PackV (Con "False" []) []
+
+unit :: Value
+unit = PackV (Con "Tuple0" []) []
