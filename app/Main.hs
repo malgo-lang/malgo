062b88b7bba806aa2a8a9769afe4a8f64c345dd6
Yuya Kono <takohati0821@gmail.com>
2020-06-02 14:22:39 +0900

    interpretオプションを渡したときにCore経由のコード生成を行うように変更

--- app/Main.hs
+++ app/Main.hs
@@ -17,8 +17,11 @@ main = do
   opt <- parseOpt
   src <- T.readFile (srcName opt)
   if isInterpretMode opt
-    then void $ interpret opt src
+    then do
+      ll' <- compileCore opt src
+      TL.putStrLn (ppllvm ll')
+      void $ interpret opt src
     else do
-      -- ll <- compile opt src
-      ll <- compileCore opt src
+      ll <- compile opt src
       TL.writeFile (dstName opt) $ TL.unlines $ "source_filename = " <> TL.pack (show (srcName opt)) : TL.lines (ppllvm ll)
+      
