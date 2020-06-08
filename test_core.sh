04914eed3ef15ca4fd3ce4bc2f104e1c4de2f1f0
Yuya Kono <takohati0821@gmail.com>
2020-06-08 09:43:35 +0900

    testcases/bugを作成

--- test_core.sh
+++ test_core.sh
@@ -4,3 +4,5 @@
 mkdir ./tmp
 stack build
 ls ./testcases | grep mlg | xargs -I{} sh -c 'echo {} && stack exec malgo -- --core-mode ./testcases/{} -o ./tmp/{}.ll && clang -lgc ./examples/corelib.c ./tmp/{}.ll -o ./tmp/{}.out && ./tmp/{}.out || exit 255' && rm ./tmp/*.ll && rm ./tmp/*.out
+
+ls ./testcases/bug | grep mlg | xargs -I{} sh -c 'echo {} && stack exec malgo -- --core-mode ./testcases/bug/{} -o ./tmp/{}.ll && clang -lgc ./examples/corelib.c ./tmp/{}.ll -o ./tmp/{}.out && ./tmp/{}.out'
