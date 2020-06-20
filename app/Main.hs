942127b6c81914890e31ecc3ed9a5973a0424f48
Yuya Kono <takohati0821@gmail.com>
2020-06-20 17:06:20 +0900

    llvm-hsを使ってコード生成するように変更

--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,18 @@
 module Main where
 
 import Control.Exception (catch, displayException)
+import qualified Data.ByteString as BS
 import qualified Data.Text.IO as T
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text.Lazy.IO as TL
-import LLVM.Pretty (ppllvm)
+import LLVM.Context
+import LLVM.Module
 import Language.Malgo.Driver
 import Language.Malgo.Monad
 import Language.Malgo.Prelude
 import System.Exit (exitFailure)
 import System.IO (hPutStr, stderr)
+
+-- import LLVM.Pretty (ppllvm)
+-- import qualified Data.Text.Lazy.IO as TL
 
 main :: IO ()
 main =
@@ -24,7 +27,10 @@ main =
         then void $ interpret opt src
         else do
           ll <- (if isCoreMode opt then compileCore else compile) opt src
-          TL.writeFile (dstName opt) $ "source_filename = " <> TL.pack (show (srcName opt)) <> "\n" <> ppllvm ll
+          -- TL.writeFile (dstName opt) (ppllvm ll)
+          withContext $ \ctx -> do
+            llvm <- withModuleFromAST ctx ll moduleLLVMAssembly
+            BS.writeFile (dstName opt) llvm
   )
     `catch` \e -> do
       hPutStr stderr (displayException (e :: Bug))
