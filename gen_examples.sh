9fa926c91540c8b56c44d1e68e339cf2e133f0cb
Yuya Kono <takohati0821@gmail.com>
2020-06-20 17:06:03 +0900

    delete gen_examples.sh

--- gen_examples.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# examplesがコンパイルできるかチェック
-mkdir ./examples/gen
-rm -f ./examples/gen/*
-stack build
-ls ./examples | grep mlg | xargs -P8 -I{} sh -c 'echo {} && stack exec malgo -- ./examples/{} -o ./examples/gen/{}.ll && clang -emit-llvm -S -O2 ./examples/gen/{}.ll -o ./examples/gen/{}_opt.ll'
