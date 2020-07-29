f84e0b2ebbaad0b70101609d12225d6c262d986a
Yuya Kono <takohati0821@gmail.com>
2020-07-29 11:13:50 +0900

    update .travis.yml

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ rm ./tmp/*
 
 for file in `ls ./testcases | grep mlg`; do
   echo -e "\n=== $file no opt ==="
-  stack exec malgo -- --no-opt --no-lambdalift ./testcases/$file -o ./tmp/$file.ll && \
+  cabal exec malgo -- --no-opt --no-lambdalift ./testcases/$file -o ./tmp/$file.ll && \
   cat ./tmp/$file.ll && \
   clang -lgc ./examples/corelib.c ./tmp/$file.ll -o ./tmp/$file.out && \
   ./tmp/$file.out && \
@@ -16,7 +16,7 @@ done
 
 for file in `ls ./testcases | grep mlg`; do
   echo -e "\n=== $file ==="
-  stack exec malgo -- ./testcases/$file -o ./tmp/$file.ll && \
+  cabal exec malgo -- ./testcases/$file -o ./tmp/$file.ll && \
   clang -lgc ./examples/corelib.c ./tmp/$file.ll -o ./tmp/$file.out && \
   ./tmp/$file.out && \
   rm ./tmp/$file.ll && \
@@ -26,7 +26,7 @@ done
 
 for file in `ls ./testcases/bug | grep mlg`; do
   echo -e "\n=== $file no opt ==="
-  stack exec malgo -- --no-opt --no-lambdalift ./testcases/bug/$file -o ./tmp/$file.ll && \
+  cabal exec malgo -- --no-opt --no-lambdalift ./testcases/bug/$file -o ./tmp/$file.ll && \
   clang -lgc ./examples/corelib.c ./tmp/$file.ll -o ./tmp/$file.out && \
   ./tmp/$file.out && \
   rm ./tmp/$file.ll && \
@@ -35,7 +35,7 @@ done
 
 for file in `ls ./testcases/bug | grep mlg`; do
   echo -e "\n=== $file ==="
-  stack exec malgo -- ./testcases/bug/$file -o ./tmp/$file.ll && \
+  cabal exec malgo -- ./testcases/bug/$file -o ./tmp/$file.ll && \
   clang -lgc ./examples/corelib.c ./tmp/$file.ll -o ./tmp/$file.out && \
   ./tmp/$file.out && \
   rm ./tmp/$file.ll && \
