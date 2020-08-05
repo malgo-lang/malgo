8dfb46c4f88fb7da931f1804bded5e30941168e7
Yuya Kono <takohati0821@gmail.com>
2020-08-05 12:43:58 +0900

    build-typeをSimpleに変更

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,3 @@
 import Distribution.Simple
-import Distribution.Simple.Setup (BuildFlags)
-import Distribution.Types.HookedBuildInfo
-  ( HookedBuildInfo,
-    emptyHookedBuildInfo,
-  )
-import Hpack
 
-main = do
-  defaultMainWithHooks $
-    simpleUserHooks
-      { preBuild = \_ _ -> hpack Verbose defaultOptions >> return emptyHookedBuildInfo
-      }
+main = defaultMain
