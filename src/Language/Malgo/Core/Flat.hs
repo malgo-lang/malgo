60d10b88c1d1fb4cfd723939d3b6a7dd82ce48e3
Yuya Kono <takohati0821@gmail.com>
2020-08-19 01:08:51 +0900

    Flatのコードを整理

--- src/Language/Malgo/Core/Flat.hs
+++ src/Language/Malgo/Core/Flat.hs
@@ -19,11 +19,14 @@ data Flat
 
 instance Pass Flat (Exp (Id CType)) (Exp (Id CType)) where
   passName = "flat"
-  trans e = uncurry (flip appEndo) <$> runWriterT (flatExp e)
+  trans e = runFlat (flatExp e)
+
+runFlat :: Monad m => WriterT (Endo r) m r -> m r
+runFlat = liftM (uncurry (flip appEndo)) . runWriterT
 
 flatExp :: Monad m => Exp a -> WriterT (Endo (Exp a)) m (Exp a)
 flatExp (Let ds e) = do
-  ds <- traverse (rtraverse flatObj) ds
+  ds <- traverse (rtraverse (appObj (runFlat . flatExp))) ds
   tell $ Endo $ \k -> Let ds k
   flatExp e
 flatExp (Match v (Unpack con ps e :| [])) = do
@@ -34,22 +37,6 @@ flatExp (Match v (Bind x e :| [])) = do
   v <- flatExp v
   tell $ Endo $ \k -> Match v (Bind x k :| [])
   flatExp e
-flatExp (Match e cs) = do
-  e <- flatExp e
-  cs <- traverse flatCase cs
-  pure $ Match e cs
+flatExp (Match e cs) =
+  Match <$> flatExp e <*> traverse (appCase (runFlat . flatExp)) cs
 flatExp e = pure e
-
-flatObj :: Monad m => Obj a -> m (Obj a)
-flatObj (Fun ps e) = do
-  e <- uncurry (flip appEndo) <$> runWriterT (flatExp e)
-  pure $ Fun ps e
-flatObj o = pure o
-
-flatCase :: Monad m => Case a -> m (Case a)
-flatCase (Unpack con ps e) = do
-  e <- uncurry (flip appEndo) <$> runWriterT (flatExp e)
-  pure $ Unpack con ps e
-flatCase (Bind x e) = do
-  e <- uncurry (flip appEndo) <$> runWriterT (flatExp e)
-  pure $ Bind x e
