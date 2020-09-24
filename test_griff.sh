ef5c2f30d6743fcff8328346d489770525cea9d1
Yuya Kono <takohati0821@gmail.com>
2020-09-24 22:04:01 +0900

    add test_griff.sh

---
+++ test_griff.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+TESTDIR=/tmp/malgo_test
+mkdir $TESTDIR
+
+for file in `ls ./examples/griff | grep Test`; do
+  cabal exec griff -- ./examples/griff/$file -o $TESTDIR/$file.ll && \
+  cat $TESTDIR/$file.ll && \
+  clang -lgc ./examples/griff/lib.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
+  $TESTDIR/$file.out || exit 255
+  echo 'SUCCESS!!'
+done
