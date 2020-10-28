27dd731fc5024607692a8569d7c4d299f8909d1d
Yuya Kono <takohati0821@gmail.com>
2020-10-29 08:26:47 +0900

    update test_griff.sh

--- test_griff.sh
+++ test_griff.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-TESTDIR=/tmp/malgo_test
+TESTDIR=/tmp/griff_test
 mkdir $TESTDIR
 
 for file in `ls ./examples/griff | grep Test`; do
+  echo $file;
   cabal exec griffc -- ./examples/griff/$file -o $TESTDIR/$file.ll && \
   clang -lgc ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
   $TESTDIR/$file.out || exit 255
