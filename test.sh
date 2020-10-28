3f2f1093bc0409bd8a1b0bfeb264ff4baf57f724
Yuya Kono <takohati0821@gmail.com>
2020-10-28 21:28:46 +0900

    実行ファイルの名前をmalgoc, griffcに変更

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ mkdir $TESTDIR
 # testcasesがコンパイルできるかチェック
 for file in `ls ./testcases | grep mlg`; do
   echo -e "\n=== $file no opt ==="
-  cabal exec malgo -- --no-opt --no-lambdalift ./testcases/$file -o $TESTDIR/$file.ll && \
+  cabal exec malgoc -- --no-opt --no-lambdalift ./testcases/$file -o $TESTDIR/$file.ll && \
   cat $TESTDIR/$file.ll && \
   clang -lgc ./runtime/malgo/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
   $TESTDIR/$file.out || exit 255
@@ -15,7 +15,7 @@ done
 
 for file in `ls ./testcases | grep mlg`; do
   echo -e "\n=== $file ==="
-  cabal exec malgo -- ./testcases/$file -o $TESTDIR/$file.ll && \
+  cabal exec malgoc -- ./testcases/$file -o $TESTDIR/$file.ll && \
   clang -lgc ./runtime/malgo/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
   $TESTDIR/$file.out || exit 255
   echo 'SUCCESS!!'
@@ -23,14 +23,14 @@ done
 
 for file in `ls ./testcases/bug | grep mlg`; do
   echo -e "\n=== $file no opt ==="
-  cabal exec malgo -- --no-opt --no-lambdalift ./testcases/bug/$file -o $TESTDIR/$file.ll && \
+  cabal exec malgoc -- --no-opt --no-lambdalift ./testcases/bug/$file -o $TESTDIR/$file.ll && \
   clang -lgc ./runtime/malgo/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
   $TESTDIR/$file.out || echo 'FAIL!!'
 done
 
 for file in `ls ./testcases/bug | grep mlg`; do
   echo -e "\n=== $file ==="
-  cabal exec malgo -- ./testcases/bug/$file -o $TESTDIR/$file.ll && \
+  cabal exec malgoc -- ./testcases/bug/$file -o $TESTDIR/$file.ll && \
   clang -lgc ./runtime/malgo/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
   $TESTDIR/$file.out || echo 'FAIL!!'
 done
--- test_griff.sh
+++ test_griff.sh
@@ -4,7 +4,7 @@ TESTDIR=/tmp/malgo_test
 mkdir $TESTDIR
 
 for file in `ls ./examples/griff | grep Test`; do
-  cabal exec griff -- ./examples/griff/$file -o $TESTDIR/$file.ll && \
+  cabal exec griffc -- ./examples/griff/$file -o $TESTDIR/$file.ll && \
   clang -lgc ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
   $TESTDIR/$file.out || exit 255
   echo 'SUCCESS!!'
