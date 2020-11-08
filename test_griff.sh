a6a9c3e5a99e69e08499ab413d7fadafb8867f9c
Yuya Kono <takohati0821@gmail.com>
2020-11-08 18:36:56 +0900

    最適化なしの場合もテストする

--- test_griff.sh
+++ test_griff.sh
@@ -3,6 +3,16 @@
 TESTDIR=/tmp/griff_test
 mkdir $TESTDIR
 
+echo '=== no opt ==='
+for file in `ls ./examples/griff`; do
+  echo $file;
+  cabal exec griffc -- --no-opt ./examples/griff/$file -o $TESTDIR/$file.ll && \
+  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
+  $TESTDIR/$file.out || exit 255
+  echo 'SUCCESS!!'
+done
+
+echo '=== opt ==='
 for file in `ls ./examples/griff`; do
   echo $file;
   cabal exec griffc -- ./examples/griff/$file -o $TESTDIR/$file.ll && \
