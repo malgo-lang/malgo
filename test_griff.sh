24c080c4b2976fda25338e9b044d3b722a099b2f
Yuya Kono <takohati0821@gmail.com>
2020-11-09 15:44:21 +0900

    標準出力への出力が想定と一致しているかをtest_griff.shで検査

--- test_griff.sh
+++ test_griff.sh
@@ -6,35 +6,43 @@ mkdir $TESTDIR
 echo '=== no opt no lambdalift ==='
 for file in `ls ./examples/griff`; do
   echo $file;
-  cabal exec griffc -- --no-opt --no-lambdalift ./examples/griff/$file -o $TESTDIR/$file.ll && \
-  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
+  cat ./examples/griff/$file | grep -q '^-- Expected: ' || exit 255
+  cabal exec griffc -- --no-opt --no-lambdalift ./examples/griff/$file -o $TESTDIR/$file.ll || exit 255
+  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out || exit 255
   $TESTDIR/$file.out || exit 255
+  test "$($TESTDIR/$file.out)" = "$(cat ./examples/griff/$file | grep '^-- Expected: ' | sed -e 's/^-- Expected: //')" || exit 255
   echo 'SUCCESS!!'
 done
 
 echo '=== no opt ==='
 for file in `ls ./examples/griff`; do
   echo $file;
-  cabal exec griffc -- --no-opt ./examples/griff/$file -o $TESTDIR/$file.ll && \
-  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
+  cat ./examples/griff/$file | grep -q '^-- Expected: ' || exit 255
+  cabal exec griffc -- --no-opt ./examples/griff/$file -o $TESTDIR/$file.ll || exit 255
+  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out || exit 255
   $TESTDIR/$file.out || exit 255
+  test "$($TESTDIR/$file.out)" = "$(cat ./examples/griff/$file | grep '^-- Expected: ' | sed -e 's/^-- Expected: //')" || exit 255
   echo 'SUCCESS!!'
 done
 
 echo '=== no lambdalift ==='
 for file in `ls ./examples/griff`; do
   echo $file;
-  cabal exec griffc -- --no-lambdalift ./examples/griff/$file -o $TESTDIR/$file.ll && \
-  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
+  cat ./examples/griff/$file | grep -q '^-- Expected: ' || exit 255
+  cabal exec griffc -- --no-lambdalift ./examples/griff/$file -o $TESTDIR/$file.ll || exit 255
+  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out || exit 255
   $TESTDIR/$file.out || exit 255
+  test "$($TESTDIR/$file.out)" = "$(cat ./examples/griff/$file | grep '^-- Expected: ' | sed -e 's/^-- Expected: //')" || exit 255
   echo 'SUCCESS!!'
 done
 
 echo '=== opt ==='
 for file in `ls ./examples/griff`; do
   echo $file;
-  cabal exec griffc -- ./examples/griff/$file -o $TESTDIR/$file.ll && \
-  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out && \
+  cat ./examples/griff/$file | grep -q '^-- Expected: ' || exit 255
+  cabal exec griffc -- ./examples/griff/$file -o $TESTDIR/$file.ll || exit 255
+  clang $(pkg-config bdw-gc --libs --cflags) ./runtime/griff/rts.c $TESTDIR/$file.ll -o $TESTDIR/$file.out || exit 255
   $TESTDIR/$file.out || exit 255
+  test "$($TESTDIR/$file.out)" = "$(cat ./examples/griff/$file | grep '^-- Expected: ' | sed -e 's/^-- Expected: //')" || exit 255
   echo 'SUCCESS!!'
 done
