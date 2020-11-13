ebfeb170cc74ebcc6415951ea9944ffb0d12f0c6
Yuya Kono <takohati0821@gmail.com>
2020-11-13 17:18:27 +0900

    不要なプラグマを削除

--- koriel/src/Koriel/Core/Optimize.hs
+++ koriel/src/Koriel/Core/Optimize.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
