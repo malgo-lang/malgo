cfa8643bdac73ae6093763e6f332dfc359bb9a09
Yuya Kono <takohati0821@gmail.com>
2021-02-09 13:00:47 +0900

    [WIP] CLIのコマンドを変更

--- malgo/app/Main.hs
+++ malgo/app/Main.hs
@@ -15,6 +15,7 @@ import Language.Malgo.Driver (compile)
 import Language.Malgo.Prelude hiding ((<.>))
 import System.Console.GetOpt
 import System.Directory (XdgDirectory (XdgData), getXdgDirectory)
+import System.Environment (getArgs)
 
 data Flags
   = DumpParsed
@@ -52,7 +53,6 @@ flags =
 data BuildOption = BuildOption
   { -- | working directory (default: .malgo-work/)
     workDir :: FilePath,
-    buildCommand :: Command,
     -- | files that need to be compiled to .ll
     sourceFiles :: [FilePath],
     -- | \$XDG_DATA_HOME/malgo/rts.c
@@ -60,11 +60,6 @@ data BuildOption = BuildOption
     malgoOpt :: Opt
   }
   deriving stock (Show)
-
-data Command
-  = Compile
-  | Link FilePath
-  deriving stock (Eq, Show)
 
 data Dep = Dep FilePath String
   deriving stock (Eq, Show)
@@ -77,12 +72,6 @@ parseBuildOption flags args = do
   pure
     BuildOption
       { workDir = workDir,
-        buildCommand =
-          if
-              | elem "compile" args && elem "build" args -> error "invalid command"
-              | elem "compile" args -> Compile
-              | elem "build" args -> Link (getOutputName args)
-              | otherwise -> error "invalid command",
         sourceFiles = filter ("mlg" `isExtensionOf`) args,
         runtimePath = runtimeDirectory </> "rts.c",
         malgoOpt =
@@ -102,11 +91,6 @@ parseBuildOption flags args = do
               modulePaths = mapMaybe (preview _ModulePath) flags
             }
       }
-  where
-    getOutputName args = normaliseEx $
-      case List.delete "build" args of
-        [] -> "a.out" <.> exe
-        (x : _) -> x <.> exe
 
 linkTo :: BuildOption -> FilePath -> Rules ()
 linkTo buildOption output = do
@@ -119,17 +103,15 @@ linkTo buildOption output = do
     need (runtimePath buildOption : targetFiles <> depLLVMFiles)
     cmd_ (concat $ ["clang $(pkg-config bdw-gc --libs --cflags) "] <> needFiles <> [" -o ", output])
 
-simpleCompile :: BuildOption -> Rules ()
-simpleCompile buildOption = do
-  for_ (sourceFiles buildOption) $ \sourceFile -> do
-    -- .ll
-    let targetFile = getTargetFile buildOption sourceFile
-    -- .mlgi
-    let interfaceFile = getInterfaceFile buildOption sourceFile
-    [targetFile, interfaceFile] |%> \_ -> do
-      let dependencies = getDependencies buildOption sourceFile
-      need (sourceFile : map toInterfaceFile dependencies)
-      liftIO $ compile (malgoOpt buildOption)
+simpleCompile :: BuildOption -> FilePath -> Rules ()
+simpleCompile buildOption sourceFile = do
+  let targetFile = getTargetFile buildOption sourceFile
+  -- .mlgi
+  let interfaceFile = getInterfaceFile buildOption sourceFile
+  [targetFile, interfaceFile] |%> \_ -> do
+    let dependencies = getDependencies buildOption sourceFile
+    need (sourceFile : map toInterfaceFile dependencies)
+    liftIO $ compile sourceFile targetFile (malgoOpt buildOption)
 
 -- \$XDG_DATA_HOME/malgo/**/にある.mlgのうち、
 -- モジュール名がsourceFilesと一致しないものをすべてコンパイルして
@@ -162,17 +144,21 @@ getTargetFile :: BuildOption -> FilePath -> FilePath
 getTargetFile buildOption sourceFile = workDir buildOption </> "build" </> takeFileName sourceFile -<.> "ll"
 
 main :: IO ()
-main = shakeArgsWith shakeOptions {shakeFiles = ".malgo-work"} flags $ \flags args -> pure $
-  Just $ do
-    buildOption <- parseBuildOption flags args
-    case buildCommand buildOption of
-      Compile -> do
-        compileInstalledModules buildOption
-        simpleCompile buildOption
-        let targetFiles = map (getTargetFile buildOption) (sourceFiles buildOption)
-        want targetFiles
-      Link output -> do
-        compileInstalledModules buildOption
-        simpleCompile buildOption
-        linkTo buildOption output
-        want [output]
+main = shakeArgsOptionsWith shakeOptions {shakeFiles = ".malgo-work"} flags $ \_ flags args -> do
+  clArgs <- liftIO getArgs
+  case clArgs of
+    ("only-compile" : sourceFile : _) ->
+      pure $ Just (shakeOptions', onlyCompileRule sourceFile flags args >> want ["only-compile"] >> addHelpSuffix helpMessage)
+    _ -> pure Nothing
+  where
+    onlyCompileRule sourceFile flags args = do
+      buildOption <- parseBuildOption flags args
+      compileInstalledModules buildOption
+      simpleCompile buildOption sourceFile
+      phony "only-compile" do
+        let targetFile = getTargetFile buildOption sourceFile
+        need [targetFile]
+    shakeOptions' = shakeOptions {shakeFiles = ".malgo-work"}
+
+helpMessage :: String
+helpMessage = "EMPTY"
