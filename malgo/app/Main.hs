2f78f7613f88fbb30dbbbeb37dac7d237e290e2a
Yuya Kono <takohati0821@gmail.com>
2021-08-13 14:24:21 +0900

    fix: prettyprint `bug $ Unreachable reason`

--- malgo/app/Main.hs
+++ malgo/app/Main.hs
@@ -12,7 +12,7 @@ import Text.Megaparsec (errorBundlePretty)
 import Text.Read (read)
 
 main :: IO ()
-main = do
+main = handleBug do
   command <- parseCommand
   case command of
     ToLL opt -> do
