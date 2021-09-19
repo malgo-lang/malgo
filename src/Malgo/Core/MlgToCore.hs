c411a60b1589563cadef59cc2a844ac395d90449
Yuya Kono <takohati0821@gmail.com>
2021-09-19 13:01:56 +0900

    wip: mlgToCore

--- src/Malgo/Core/MlgToCore.hs
+++ src/Malgo/Core/MlgToCore.hs
@@ -1,6 +1,8 @@
 module Malgo.Core.MlgToCore (mlgToCore) where
 
-import Control.Lens (At (at), Lens', lens, use, (%=), (^.))
+import Control.Lens (At (at), Lens', lens, preuse, use, (%=), (^.), _Just)
+import qualified Data.List as List
+import Data.Maybe (fromJust)
 import Koriel.Id
 import Koriel.MonadUniq (HasUniqSupply)
 import Malgo.Core.Syntax
@@ -11,26 +13,35 @@ import qualified Malgo.Syntax as S
 import Malgo.Syntax.Extension (Malgo, MalgoPhase (Refine), RnId)
 import qualified Malgo.TypeRep as T
 
-data DsEnv = DsEnv {_buildingModule :: Module, _tcEnv :: TcEnv}
+data DsEnv = DsEnv
+  { _buildingModule :: Module, -- 脱糖の結果が詰め込まれるModule
+    _interned :: HashMap RnId Name, -- インターン済みのシンボル
+    _tcEnv :: TcEnv
+  }
 
 buildingModule :: Lens' DsEnv Module
 buildingModule = lens _buildingModule \e x -> e {_buildingModule = x}
+
+interned :: Lens' DsEnv (HashMap RnId Name)
+interned = lens _interned \e x -> e {_interned = x}
 
 tcEnv :: Lens' DsEnv TcEnv
 tcEnv = lens _tcEnv \e x -> e {_tcEnv = x}
 
 mlgToCore :: (MonadIO m, MonadReader env m, HasUniqSupply env, MonadFail m) => TcEnv -> S.Module (Malgo 'Refine) -> m Module
-mlgToCore tcEnv (S.Module _ S.BindGroup {_scDefs}) = evaluatingStateT
+mlgToCore tcEnv (S.Module _ S.BindGroup {_scDefs, _dataDefs}) = evaluatingStateT
   DsEnv
     { _buildingModule = Module {_variableDefinitions = [], _externalDefinitions = [], _typeDefinitions = []},
+      _interned = mempty,
       _tcEnv = tcEnv
     }
   do
+    -- dsTypeDef =<< use (tcEnv . TcEnv.typeEnv)
     traverse_ dsScDefs _scDefs
     use buildingModule
 
-dsNewVarName :: (MonadState DsEnv m, MonadIO m, HasUniqSupply env, MonadReader env m, MonadFail m) => RnId -> m Name
-dsNewVarName old = do
+dsVarName :: (MonadState DsEnv m, MonadIO m, HasUniqSupply env, MonadReader env m, MonadFail m) => RnId -> m Name
+dsVarName old = do
   Just scheme <- use (tcEnv . TcEnv.varEnv . at old)
   scheme <- dsScheme scheme
   newIdOnName scheme old
@@ -50,7 +61,7 @@ dsScDefs = traverse_ dsScDef
 
 dsScDef :: (MonadState DsEnv m, MonadIO m, HasUniqSupply env, MonadReader env m, MonadFail m) => S.ScDef (Malgo 'Refine) -> m ()
 dsScDef (_, name, expr) = do
-  name <- dsNewVarName name
+  name <- dsVarName name
   expr <- dsExp expr (name ^. idMeta)
   buildingModule . variableDefinitions %= ((name, expr) :)
 
