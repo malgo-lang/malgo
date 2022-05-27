6f3e149fc0e707168a433c6353f6efce86e848d1
Yuya Kono <takohati0821@gmail.com>
2022-05-27 12:35:32 +0900

    Update pretest.sh

--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -17,5 +17,7 @@ else
 fi
 
 eval "$BUILD exec malgo -- to-ll --force -M $TESTDIR/libs ./runtime/malgo/Builtin.mlg -o $TESTDIR/libs/Builtin.ll"
+ls /tmp/malgo_test/libs/
 eval "$BUILD exec malgo -- to-ll --force -M $TESTDIR/libs ./runtime/malgo/Prelude.mlg -o $TESTDIR/libs/Prelude.ll"
+ls /tmp/malgo_test/libs/
 cp ./runtime/malgo/runtime.c $TESTDIR/libs/runtime.c
