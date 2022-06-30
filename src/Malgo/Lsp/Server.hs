28c2c6ac06d8ce3d44d05e75cd8e32ddd3784c00
Yuya Kono <takohati0821@gmail.com>
2022-06-30 13:59:30 +0900

    Update Server.hs

--- src/Malgo/Lsp/Server.hs
+++ src/Malgo/Lsp/Server.hs
@@ -1,4 +1,4 @@
-module Malgo.Lsp.Server where
+module Malgo.Lsp.Server (server) where
 
 import Control.Lens (to, view, (^.))
 import qualified Data.HashMap.Strict as HashMap
@@ -9,27 +9,16 @@ import Language.LSP.Server
 import Language.LSP.Types
 import Language.LSP.Types.Lens (HasUri (uri))
 import Malgo.Interface
-import Malgo.Lsp.Index (Info (..), findInfosOfPos)
+import Malgo.Lsp.Index (Index, Info (..), findInfosOfPos)
 import Malgo.Lsp.Pass (LspOpt)
-import Malgo.Parser (parseMalgo)
 import Malgo.Prelude hiding (Range)
-import Malgo.Syntax
-import Malgo.Syntax.Extension
 import qualified Relude.Unsafe as Unsafe
 import System.FilePath (dropExtensions, takeFileName)
-import Text.Megaparsec (errorBundlePretty)
 
 textDocumentIdentifierToModuleName :: TextDocumentIdentifier -> ModuleName
 textDocumentIdentifierToModuleName (uriToFilePath . view uri -> Just filePath) =
   ModuleName $ toText $ takeFileName $ dropExtensions filePath
 textDocumentIdentifierToModuleName _ = error "textDocumentIdentifierToModuleName: invalid TextDocumentIdentifier"
-
-readAst :: MonadIO m => LspOpt -> FilePath -> m (Module (Malgo 'Parse))
-readAst _opt filePath = do
-  src <- decodeUtf8 <$> readFileBS filePath
-  case parseMalgo filePath src of
-    Left err -> error $ toText $ "readAst: " <> errorBundlePretty err
-    Right ast -> pure ast
 
 handlers :: LspOpt -> Handlers (LspM ())
 handlers opt =
@@ -39,43 +28,30 @@ handlers opt =
       -- textDocument/hover
       requestHandler STextDocumentHover $ \req responder -> do
         let RequestMessage _ _ _ (HoverParams doc pos _workDone) = req
-        _ast <- readAst opt (Unsafe.fromJust $ doc ^. uri . to uriToFilePath)
-        minterface <- liftIO $ runReaderT (loadInterface $ textDocumentIdentifierToModuleName doc) opt
-        let index = case minterface of
-              Nothing -> mempty
-              Just interface -> interface ^. lspIndex
-        let infos = findInfosOfPos (positionToSourcePos (Unsafe.fromJust $ doc ^. uri . to uriToFilePath) pos) index
-        case infos of
+        index <- loadIndex doc opt
+        case findInfosOfPos (positionToSourcePos (Unsafe.fromJust $ doc ^. uri . to uriToFilePath) pos) index of
           [] -> responder (Right Nothing)
-          _ -> do
-            let Position _l _c' = pos
-                rsp = Hover ms (Just range)
+          infos -> do
+            let rsp = Hover ms (Just range)
                 ms = HoverContents $ toHoverDocument infos
                 range = Range pos pos
             responder (Right $ Just rsp),
       -- textDocument/definition
       requestHandler STextDocumentDefinition $ \req responder -> do
         let RequestMessage _ _ _ (DefinitionParams doc pos _workDone _partialResult) = req
-        _ast <- readAst opt (Unsafe.fromJust $ doc ^. uri . to uriToFilePath)
-        minterface <- liftIO $ runReaderT (loadInterface $ textDocumentIdentifierToModuleName doc) opt
-        let index = case minterface of
-              Nothing -> mempty
-              Just interface -> interface ^. lspIndex
+        index <- loadIndex doc opt
         let infos = findInfosOfPos (positionToSourcePos (Unsafe.fromJust $ doc ^. uri . to uriToFilePath) pos) index
-        case infos of
-          [] -> responder (Right $ InR $ InL $ Language.LSP.Types.List [])
-          _ -> do
-            let rsp = InR (InL $ Language.LSP.Types.List $ concatMap infoToLocation infos)
-            responder (Right rsp),
+            rsp = InR $ InL $ Language.LSP.Types.List $ concatMap infoToLocation infos
+        responder (Right rsp),
       requestHandler STextDocumentDocumentSymbol $ \req responder -> do
         let RequestMessage _ _ _ (DocumentSymbolParams _ _ doc) = req
-        minterface <- liftIO $ runReaderT (loadInterface $ textDocumentIdentifierToModuleName doc) opt
-        let index = case minterface of
-              Nothing -> mempty
-              Just interface -> interface ^. lspIndex
+        index <- loadIndex doc opt
         let documentSymbol = HashMap.elems $ HashMap.filterWithKey (\k _ -> idIsExternal k) $ index ^. symbolInfo
         responder $ Right $ InL $ Language.LSP.Types.List documentSymbol
     ]
+
+loadIndex :: MonadIO f => TextDocumentIdentifier -> LspOpt -> f Index
+loadIndex doc opt = maybe mempty (view lspIndex) <$> runReaderT (loadInterface $ textDocumentIdentifierToModuleName doc) opt
 
 toHoverDocument :: [Info] -> MarkupContent
 toHoverDocument infos =
