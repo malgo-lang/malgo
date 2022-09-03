2b0903845559c7f5675ce8c8c677810dc886f28c
Yuya Kono <takohati0821@gmail.com>
2022-09-04 06:47:01 +0900

    Add storeIndex and loadIndex

--- src/Malgo/Lsp/Index.hs
+++ src/Malgo/Lsp/Index.hs
@@ -5,15 +5,20 @@
 
 module Malgo.Lsp.Index where
 
+import Control.Lens (view)
 import Control.Lens.TH
 import Data.HashMap.Strict qualified as HashMap
-import Data.Store (PeekException, Store, decode, encode)
+import Data.Store (Store, encode)
+import Data.Store qualified as Store
+import Data.String.Conversions (convertString)
+import Koriel.Id (ModuleName (..))
+import Koriel.Lens (HasModulePaths (modulePaths))
 import Koriel.Pretty
 import Malgo.Infer.TypeRep (Scheme, Type)
 import Malgo.Prelude
 import Malgo.Syntax.Extension (RnId)
-import System.Directory (createDirectoryIfMissing)
-import System.FilePath (takeDirectory, takeFileName)
+import System.Directory qualified as Directory
+import System.FilePath (takeFileName, (-<.>), (</>))
 import Text.Megaparsec.Pos (Pos, SourcePos (..))
 import Text.Pretty.Simple (pShow)
 
@@ -81,10 +86,27 @@ isInRange pos Range {_start, _end}
     posToTuple :: SourcePos -> (Pos, Pos)
     posToTuple SourcePos {sourceLine, sourceColumn} = (sourceLine, sourceColumn)
 
-storeIndex :: FilePath -> Index -> IO ()
-storeIndex path index = do
-  createDirectoryIfMissing True (takeDirectory path)
-  writeFileBS path (encode index)
+storeIndex :: (MonadReader s m, HasDstName s FilePath, Store a, MonadIO m) => a -> m ()
+storeIndex index = do
+  dstName <- view dstName
+  let encoded = encode index
+  writeFileBS (dstName -<.> "idx") encoded
 
-loadIndex :: FilePath -> IO (Either PeekException Index)
-loadIndex path = decode <$> readFileBS path
+loadIndex :: (MonadReader s m, MonadIO m, HasModulePaths s [FilePath]) => ModuleName -> m (Maybe Index)
+loadIndex modName = do
+  modPaths <- view modulePaths
+  message <- findAndReadFile modPaths (convertString modName.raw <> ".idx")
+  case message of
+    Right x -> pure $ Just x
+    Left err -> do
+      hPrint stderr err
+      pure Nothing
+  where
+    findAndReadFile [] modFile = pure $ Left $ "Cannot find " <> modFile
+    findAndReadFile (modPath : rest) modFile = do
+      isExistModFile <- liftIO $ Directory.doesFileExist (modPath </> modFile)
+      if isExistModFile
+        then do
+          raw <- readFileBS (modPath </> modFile)
+          Right <$> liftIO (Store.decodeIO raw)
+        else findAndReadFile rest modFile
