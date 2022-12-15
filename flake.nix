8c64a6cef7f049c5beb32ee5702422886c6d5095
Yuya Kono <takohati0821@gmail.com>
2022-12-15 22:32:11 +0900

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,55 @@
 {
   description = "malgo flake";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [ haskellNix.overlay
-        (final: prev: {
-          # This overlay adds our project to pkgs
-          malgo =
-            final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc925";
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = {};
-                hlint = {};
-                haskell-language-server = {};
-              };
-              # Non-Haskell shell tools go here
-              shell.buildInputs = with pkgs; [
-                nixpkgs-fmt
-              ];
-              # This adds `js-unknown-ghcjs-cabal` to the shell.
-              # shell.crossPlatforms = p: [p.ghcjs];
-            };
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-      flake = pkgs.malgo.flake {
-        # This adds support for `nix build .#js-unknown-ghcjs:hello:exe:hello`
-        # crossPlatforms = p: [p.ghcjs];
-      };
-    in flake // {
-      # Built by `nix build .`
-      packages.default = flake.packages."malgo:exe:malgo";
-    });
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "malgo";
+
+        llvm-hs-repo = pkgs.fetchFromGitHub {
+          owner = "MuKnIO";
+          repo = "llvm-hs";
+          rev = "fe8fd556e8d2cc028f61d4d7b4b6bf18c456d090";
+          sha256 = "1b4kh0c8sgb39qzfi3cnagz1ssgl96mzb6s18xapy0qj23zql0p0";
+        };
+        llvm-hs = (haskellPackages.callCabal2nix "llvm-hs" "${llvm-hs-repo}/llvm-hs" {
+          inherit llvm-hs-pure;
+        }).overrideAttrs (oldAttrs: rec {
+          buildInputs = oldAttrs.buildInputs ++ [
+            pkgs.llvm_12
+          ];
+        });
+        llvm-hs-pure = haskellPackages.callCabal2nix "llvm-hs-pure" "${llvm-hs-repo}/llvm-hs-pure" {};
+      in {
+        packages.${packageName} = # (ref:haskell-package-def)
+          haskellPackages.callCabal2nix packageName self rec {
+            # Dependency overrides go here
+            inherit llvm-hs;
+            inherit llvm-hs-pure;
+          };
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            ghcid
+            cabal-install
+          ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
 }
+
