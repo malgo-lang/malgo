1fb3e42001d7adb20f272a32fc17542c88aac753
Yuya Kono <takohati0821@gmail.com>
2023-06-09 05:18:30 +0000

    format

--- src/Koriel/Core/Syntax.hs
+++ src/Koriel/Core/Syntax.hs
@@ -250,17 +250,17 @@ data Expr a
   | -- | switch expression
     Switch
       (Atom a)
-      -- | cases
       [(Tag, Expr a)]
-      -- | default case
+      -- ^ cases
       (Expr a)
+      -- ^ default case
   | -- | switch by unboxed value
     SwitchUnboxed
       (Atom a)
-      -- | cases
       [(Unboxed, Expr a)]
-      -- | default case
+      -- ^ cases
       (Expr a)
+      -- ^ default case
   | -- | destruct a value
     Destruct (Atom a) Con [a] (Expr a)
   | -- | destruct a record
