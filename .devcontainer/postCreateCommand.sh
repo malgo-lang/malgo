f1271c2dd5047a0026ad67038090f8cb4937b901
Yuya Kono <takohati0821@gmail.com>
2023-08-27 12:35:12 +0000

    Update haskell-language-server

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
-ghcup compile hls --git-ref 7860df3e97ec5a286697aeed3f05a9ab08e7617e --ghc 9.6.2 -j$(nproc)
+ghcup compile hls --git-ref 90d71cee120bd50473098c8522820fce4418ede5 --ghc 9.6.2 -j$(nproc)
 # ghcup install hls
 stack install ghc-prof-flamegraph # --ignore-project
 stack install hlint # --ignore-project
