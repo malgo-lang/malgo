0e6a01601d5cf2f4198f09e63c5d52d037d42886
Yuya Kono <takohati0821@gmail.com>
2023-08-29 08:04:26 +0000

    Merge tests

--- test/Malgo/ParserTest.hs
+++ test/Malgo/ParserTest.hs
@@ -8,38 +8,19 @@ import Malgo.Prelude
 import Test.Tasty (TestTree)
 import Test.Tasty.Golden (goldenVsString)
 
-test_var :: TestTree
-test_var = goldenVsString "parse variable" "test/Malgo/Parser/golden/var.golden" do
-  case parse "test.mlg" "x" of
-    Left err -> pure $ convertString err
-    Right result -> pure $ convertString $ show result
-
-test_lit :: TestTree
-test_lit = goldenVsString "parse literal" "test/Malgo/Parser/golden/lit.golden" do
-  case parse "test.mlg" "42" of
-    Left err -> pure $ convertString err
-    Right result -> pure $ convertString $ show result
-
-test_apply :: TestTree
-test_apply = goldenVsString "parse apply" "test/Malgo/Parser/golden/apply.golden" do
-  case parse "test.mlg" "f x y z" of
-    Left err -> pure $ convertString err
-    Right result -> pure $ convertString $ show result
-
-test_this :: TestTree
-test_this = goldenVsString "parse this" "test/Malgo/Parser/golden/this.golden" do
-  case parse "test.mlg" "#" of
-    Left err -> pure $ convertString err
-    Right result -> pure $ convertString $ show result
-
-test_comment :: TestTree
-test_comment = goldenVsString "parse comment" "test/Malgo/Parser/golden/comment.golden" do
-  case parse "test.mlg" "# {- {- comment -} comment -}" of
-    Left err -> pure $ convertString err
-    Right result -> pure $ convertString $ show result
-
-test_function :: TestTree
-test_function = goldenVsString "parse function" "test/Malgo/Parser/golden/function.golden" do
-  case parse "test.mlg" "{ # x -> x }" of
-    Left err -> pure $ convertString err
-    Right result -> pure $ convertString $ show result
+test_parser :: [TestTree]
+test_parser =
+  map
+    aux
+    [ ("variable", "x"),
+      ("literal", "42"),
+      ("apply", "f x y z"),
+      ("this", "#"),
+      ("comment", "# {- {- comment -} comment -}"),
+      ("function", "{ # x -> x }")
+    ]
+  where
+    aux (name, input) = goldenVsString ("parse " <> name) ("test/Malgo/Parser/golden/" <> name <> ".golden") do
+      case parse "test.mlg" input of
+        Left err -> pure $ convertString err
+        Right result -> pure $ convertString $ show result
