d83c4191c82ba546d5356533f77cd604ed78cfe7
Yuya KONO <takohati0821@gmail.com>
2025-01-10 08:35:50 +0000

    Disable HLS installation in Dockerfile and update post-create command to compile HLS

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,3 +1,5 @@
 /home/vscode/.local/bin/mise trust && /home/vscode/.local/bin/mise install && /home/vscode/.local/bin/mise run setup
 
 cabal update && cabal install hpack
+
+ghcup compile hls --git-ref master --ghc 9.10.1 -j 4
