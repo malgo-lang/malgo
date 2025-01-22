2cbaa7ff62b39d9d4d77417ac28a401969f049c6
Yuya KONO <takohati0821@gmail.com>
2025-01-22 09:55:33 +0000

    Update DevContainer configuration

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,1 +1,5 @@
-cargo install git-cliff
+/home/vscode/.local/bin/mise trust && /home/vscode/.local/bin/mise install && /home/vscode/.local/bin/mise run setup
+
+cabal update && cabal install hpack
+
+ghcup compile hls --git-ref master --ghc 9.6.4 -j 4
