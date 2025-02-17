1823b26afb65b67aaf4cbb9babe674f3d6972f00
Yuya Kono <takohati0821@gmail.com>
2025-02-17 06:35:13 +0000

    Remove unused 'equiv' function and related definitions from Malgo.Core.Alpha module

--- src/Malgo/Core/Alpha.hs
+++ src/Malgo/Core/Alpha.hs
@@ -1,7 +1,6 @@
 -- | Alpha conversion
 module Malgo.Core.Alpha
   ( alpha,
-    equiv,
     Subst,
   )
 where
@@ -9,7 +8,7 @@ where
 import Control.Exception (assert)
 import Control.Lens (traverseOf)
 import Data.Map.Strict qualified as Map
-import Effectful (Eff, runPureEff, (:>))
+import Effectful (Eff, (:>))
 import Effectful.Reader.Static (Reader, ask, local, runReader)
 import Effectful.State.Static.Local (State)
 import Malgo.Core.Syntax
@@ -126,102 +125,3 @@ alphaCase (Bind x t e) = do
   x' <- cloneMeta x
   local (Map.insert x (Var x')) $ Bind x' t <$> alphaExpr e
 alphaCase (Exact u e) = Exact u <$> alphaExpr e
-
-equiv :: Expr (Meta Type) -> Expr (Meta Type) -> Maybe Subst
-equiv e1 e2 = runPureEff $ runReader ?? mempty $ do
-  isEquiv <- equivExpr @'[Reader Subst] e1 e2
-  if isEquiv then Just <$> ask @Subst else pure Nothing
-
-equivExpr :: (Reader Subst :> es) => Expr (Meta Type) -> Expr (Meta Type) -> Eff es Bool
-equivExpr (Atom x) (Atom y) = equivAtom x y
-equivExpr (Call f xs) (Call g ys) =
-  (&&) <$> equivAtom f g <*> andM (zipWith equivAtom xs ys)
-equivExpr (CallDirect f xs) (CallDirect g ys) = do
-  subst <- ask
-  let f' = fromMaybe (Var f) $ Map.lookup f subst
-  let g' = fromMaybe (Var g) $ Map.lookup g subst
-  rest <- andM $ zipWith equivAtom xs ys
-  pure $ f' == g' && rest
-equivExpr (RawCall n t xs) (RawCall m u ys) =
-  ((n == m && t == u) &&) <$> andM (zipWith equivAtom xs ys)
-equivExpr (Cast t x) (Cast u y) =
-  (t == u &&) <$> equivAtom x y
-equivExpr (Let ds e) (Let ds' e') = do
-  subst <- mconcat <$> zipWithM equivLocalDef ds ds'
-  if Map.null subst
-    then pure False
-    else do
-      local (subst <>) $ equivExpr e e'
-equivExpr (Match e cs) (Match e' cs') = do
-  (&&) <$> equivExpr e e' <*> andM (zipWith equivCase cs cs')
-equivExpr (Switch x cs e) (Switch y cs' e') = do
-  andM [equivAtom x y, andM $ zipWith equivAlt cs cs', equivExpr e e']
-  where
-    equivAlt (tag, e) (tag', e') | tag == tag' = equivExpr e e'
-    equivAlt _ _ = pure False
-equivExpr (SwitchUnboxed x cs e) (SwitchUnboxed y cs' e') = do
-  andM [equivAtom x y, andM $ zipWith equivAlt cs cs', equivExpr e e']
-  where
-    equivAlt (tag, e) (tag', e') | tag == tag' = equivExpr e e'
-    equivAlt _ _ = pure False
-equivExpr (Destruct x c ps e) (Destruct y c' ps' e') | c == c' = do
-  (&&)
-    <$> equivAtom x y
-    <*> local
-      (Map.fromList (zip ps $ map Var ps') <>)
-      (equivExpr e e')
-equivExpr (DestructRecord x kps e) (DestructRecord y kps' e') = do
-  (&&)
-    <$> equivAtom x y
-    <*> local
-      (Map.fromList (zip (Map.elems kps) (map Var $ Map.elems kps')) <>)
-      (equivExpr e e')
-equivExpr (Assign x e b) (Assign y e' b') =
-  (&&)
-    <$> equivExpr e e'
-    <*> local (Map.insert x (Var y)) (equivExpr b b')
-equivExpr (Error t) (Error u) = pure $ t == u
-equivExpr _ _ = pure False
-
-equivCase :: (Reader Subst :> es) => Case (Meta Type) -> Case (Meta Type) -> Eff es Bool
-equivCase (Unpack c ps e) (Unpack c' ps' e') | c == c' = do
-  local (Map.fromList (zip ps $ map Var ps') <>) $ equivExpr e e'
-equivCase (OpenRecord kps e) (OpenRecord kps' e') = do
-  local
-    (Map.fromList (zip (Map.elems kps) (map Var $ Map.elems kps')) <>)
-    $ equivExpr e e'
-equivCase (Exact u e) (Exact u' e')
-  | u == u' =
-      equivExpr e e'
-equivCase (Bind x t e) (Bind y u f) | t == u = do
-  local (Map.insert x (Var y)) $ equivExpr e f
-equivCase _ _ = pure False
-
-equivLocalDef ::
-  (Reader Subst :> es) =>
-  LocalDef (Meta Type) ->
-  LocalDef (Meta Type) ->
-  Eff es (Map (Meta Type) (Atom (Meta Type)))
-equivLocalDef (LocalDef x t o) (LocalDef y u p) =
-  local (Map.insert x (Var y))
-    $ ifM
-      ((t == u &&) <$> equivObj o p)
-      (pure $ Map.singleton x (Var y))
-      (pure mempty)
-
-equivObj :: (Reader Subst :> es) => Obj (Meta Type) -> Obj (Meta Type) -> Eff es Bool
-equivObj (Fun ps e) (Fun ps' e') = do
-  local (Map.fromList (zip ps $ map Var ps') <>) $ equivExpr e e'
-equivObj (Pack t c xs) (Pack u d ys) =
-  ((t == u && c == d) &&) <$> andM (zipWith equivAtom xs ys)
-equivObj (Record kvs) (Record kvs') =
-  andM $ Map.intersectionWith equivAtom kvs kvs'
-equivObj _ _ = pure False
-
-equivAtom :: (Reader Subst :> es) => Atom (Meta Type) -> Atom (Meta Type) -> Eff es Bool
-equivAtom (Var x) (Var y) = do
-  subst <- ask
-  let x' = fromMaybe (Var x) $ Map.lookup x subst
-  let y' = fromMaybe (Var y) $ Map.lookup y subst
-  pure $ x' == y'
-equivAtom x y = pure $ x == y
