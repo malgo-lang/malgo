a8ae1d9018a8b37af0d549b018410134ac87db42
Yuya Kono <takohati0821@gmail.com>
2025-02-18 09:08:50 +0000

    Refactor Command and Pattern data types for improved clarity and structure

--- src/Malgo/Machine/Command.hs
+++ src/Malgo/Machine/Command.hs
@@ -14,70 +14,103 @@ import Malgo.Prelude
 
 data Program = Program
   {definitions :: [(Name, Code)]}
+  deriving stock (Show)
+
+type Name = Id
 
 type Code = [Command]
+
+-- | Command is a basic unit of computation.
+data Command
+  = -- Producer
+
+    -- | @(S, E, Fetch(name) : C) -> (E[name] : S, E, C)@
+    Fetch Range Name
+  | -- | Push pushes a literal as a value.
+    --
+    -- @(S, E, Push(lit) : C) -> (lit : S, E, C)@
+    Push Range Literal
+  | -- | Construct creates a new structure with tag and push it to the stack.
+    --
+    -- @(value*n <> S, E, Construct(tag, n) : C) -> (tag(value*n) : S, E, C)@
+    Construct Range Tag Int
+  | -- | Lambda create a new (recursive) closure and push it to the stack.
+    --
+    -- @(S, E, Lambda(name*, code) : C) -> (λ E name* . code : S, E, C)@
+    Lambda Range [Name] Code
+  | -- | Object creates a new object and push it to the stack.
+    -- Unlike other languages, Object's fields are lazy.
+    --
+    -- @(S, E, Object(field*) : C) -> ({E field*} : S, E, C)@
+    Object Range (Map Text Code)
+  | -- | Do captures rest of the code and switch to the new block.
+    --
+    -- @(S, E, Do(name, code) : C) -> (S, E {name = C}, code)@
+    Do Range Name Code
+  | -- | Suspend creates a scoped label.
+    -- In other words, it turns a consumer into a producer.
+    --
+    -- @(S, E, Suspend(command) : C) -> ([E, command] : S, E, C)@
+    Suspend Range Command
+  | -- Consumer
+
+    -- | Resume jumps to a scoped label created by Suspend.
+    --
+    -- @(S, E { name = [E', command] }, Resume(name))) -> (S, E', command)@
+    Resume Range Name
+  | -- | Apply applies a function to the top of the stack.
+    --
+    -- @(value*n <> λ E name* . code : S, _, Apply(n)) -> (S, E {name* = reverse value*n}, code)@
+    Apply Range Int
+  | -- | Proj projects a field from the top of the stack.
+    --
+    -- @({E field: code, ...} : S, _, Proj(field)) -> (S, E, code)@
+    Proj Range Text
+  | -- | Then assigns a value to a variable.
+    --
+    -- @(value : S, E, Then(name, code)) -> (S, E {name = value}, code)@
+    Then Range Name Code
+  | -- | Finish finishes the evaluation.
+    --
+    -- @(value : S, E, Finish) -> exit with value@
+    Finish Range
+  | -- Statement
+
+    -- | Primitive is a primitive operation.
+    --
+    -- @(value* <> S, E, Primitive(name)) -> modified S, E, C by the primitive operation@
+    Primitive Range Text
+  | -- | Select selects a branch based on the values on the stack.
+    --
+    -- @(value* <> S, E, Select(range, branch*)) -> (S, E, selected branch)@
+    Select Range [Branch]
+  deriving stock (Show)
+
+data Literal
+  = Int32 Int32
+  | Int64 Int64
+  | Float Float
+  | Double Double
+  | Char Char
+  | String Text
+  deriving stock (Show)
+
+-- | Tag is used to distinguish different structures.
+data Tag = Tuple | Tag Text
+  deriving stock (Show)
 
 data Branch = Branch
   { range :: Range,
     patterns :: [Pattern],
     code :: Code
   }
+  deriving stock (Show)
 
--- | Command is a basic unit of computation.
-data Command where
-  -- Producer
-
-  -- | Push pushes a literal as a value.
-  Push :: Range -> Literal -> Command
-  Get :: Range -> Name -> Command
-  -- | Construct creates a new structure with tag and push it to the stack.
-  Construct ::
-    Range ->
-    Tag ->
-    -- | Number of fields.
-    Int ->
-    Command
-  -- | Lambda create a new (recursive) closure and push it to the stack.
-  Lambda :: Range -> Name -> Code -> Command
-  -- | Object creates a new object and push it to the stack.
-  -- Unlike other languages, Object's fields are lazy.
-  Object :: Range -> Map Text Code -> Command
-  Do :: Range -> Name -> Code -> Command
-  -- Consumer
-
-  -- | Apply applies a function to the top of the stack.
-  Apply :: Range -> Int -> Int -> Command
-  -- | Proj projects a field from the top of the stack.
-  Proj :: Range -> Text -> Command
-  -- | Assign assigns a value to a variable.
-  Then :: Range -> Name -> Command
-  -- Statement
-
-  -- | Primitive is a primitive operation.
-  Primitive :: Range -> Text -> Command
-  -- | Select selects a branch based on the values on the stack.
-  Select ::
-    Range ->
-    [Branch] ->
-    Command
-
--- | Tag is used to distinguish different structures.
-data Tag = Tuple | Tag Text
-
-data Literal where
-  Int32 :: Int32 -> Literal
-  Int64 :: Int64 -> Literal
-  Float :: Float -> Literal
-  Double :: Double -> Literal
-  Char :: Char -> Literal
-  String :: Text -> Literal
-
-type Name = Id
-
-data Pattern where
-  Var :: Name -> Pattern
-  Literal :: Literal -> Pattern
-  -- | Destruct a structure.
-  Destruct :: Tag -> [Pattern] -> Pattern
-  -- | Expand an object.
-  Expand :: Map Text Pattern -> Pattern
+data Pattern
+  = Var Name
+  | Literal Literal
+  | -- | Destruct a structure.
+    Destruct Tag [Pattern]
+  | -- | Expand an object.
+    Expand (Map Text Pattern)
+  deriving stock (Show)
