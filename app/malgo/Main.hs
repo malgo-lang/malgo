41afce90e3a71fbbcd9973777485d8977b9359d4
Yuya Kono <takohati0821@gmail.com>
2025-04-07 11:08:25 +0900

    fix(Main)!: simplify EvalOpt and remove unused code

--- app/malgo/Main.hs
+++ app/malgo/Main.hs
@@ -4,23 +4,17 @@
 
 module Main (main) where
 
-import Data.ByteString qualified as BS
-import Malgo.Core.Optimize (OptimizeOption (..))
-import Malgo.Core.Parser qualified as Core
 import Malgo.Driver qualified as Driver
 import Malgo.Monad (runMalgoM)
 import Malgo.Monad qualified as Flag
 import Malgo.Prelude
 import Options.Applicative
 import System.Directory (makeAbsolute)
-import System.Exit (exitFailure)
-import Text.Megaparsec (errorBundlePretty)
 
-data EvalOpt = ToLLOpt
+data EvalOpt = EvalOpt
   { srcPath :: FilePath,
     noOptimize :: Bool,
     lambdaLift :: Bool,
-    optimizeOption :: OptimizeOption,
     debugMode :: Bool
   }
 
@@ -37,49 +31,25 @@ main = do
               Flag.debugMode = opt.debugMode,
               Flag.testMode = False
             }
-          opt.optimizeOption
-    Core (CoreOpt srcPath) -> do
-      srcContents <- BS.readFile srcPath
-      case Core.parse srcPath (convertString srcContents) of
-        Left err -> do
-          hPutStrLn stderr $ errorBundlePretty err
-          exitFailure
-        Right prog -> do
-          putText $ render $ pretty prog
 
 evalOpt :: Parser EvalOpt
 evalOpt =
-  ( ToLLOpt
+  ( EvalOpt
       <$> strArgument (metavar "SOURCE" <> help "Source file (relative path)" <> action "file")
       <*> switch (long "no-opt")
       <*> switch (long "lambdalift")
-      <*> ( OptimizeOption
-              <$> switch (long "ffold-variable")
-              <*> switch (long "finline-constructor")
-              <*> switch (long "feliminate-unused-let")
-              <*> switch (long "finline-function")
-              <*> (read <$> strOption (long "finline-threshold" <> metavar "INT" <> value "10"))
-              <*> switch (long "ffold-redundant-cast")
-              <*> switch (long "ffold-trivial-call")
-              <*> (not <$> switch (long "fspecialize-function"))
-              <*> switch (long "fremove-noop-destruct")
-          )
       <*> switch (long "debug-mode")
   )
     <**> helper
 
-newtype CoreOpt = CoreOpt FilePath
-  deriving stock (Eq, Show)
-
 data Command
   = Eval EvalOpt
-  | Core CoreOpt
 
 parseCommand :: IO Command
 parseCommand = do
   command <-
     execParser
-      ( info ((subparser toLL <|> subparser core) <**> helper)
+      ( info (subparser eval <**> helper)
           $ fullDesc
           <> header "malgo programming language"
       )
@@ -87,17 +57,9 @@ parseCommand = do
     Eval opt -> do
       srcPath <- makeAbsolute opt.srcPath
       pure $ Eval opt {srcPath = srcPath}
-    Core opt -> pure $ Core opt
   where
-    toLL =
+    eval =
       command "eval"
         $ info (Eval <$> evalOpt)
         $ fullDesc
         <> progDesc "Evaluate a malgo program"
-    core =
-      command "core"
-        $ info (Core <$> coreOpt)
-        $ fullDesc
-        <> progDesc "Core Compiler"
-        <> header "malgo core"
-    coreOpt = CoreOpt <$> strArgument (metavar "SOURCE" <> help "Source file" <> action "file")
