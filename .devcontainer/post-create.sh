cf238771e22097557d09f2d0005306dca6004a43
Yuya Kono <takohati0821@gmail.com>
2025-07-04 04:44:08 +0000

    chore: update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,1 @@
 /home/vscode/.local/bin/mise trust && /home/vscode/.local/bin/mise install && /home/vscode/.local/bin/mise run setup
-
-cabal update && cabal install hpack
-
-ghcup compile hls --git-ref master --ghc 9.10.1 -j 4
