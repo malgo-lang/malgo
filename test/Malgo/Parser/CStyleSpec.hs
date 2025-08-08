ae3ef879f5e9c35df4a914df2f5ef61fbaad7c2e
Yuya Kono <takohati0821@gmail.com>
2025-08-08 23:27:14 +0900

    Add golden tests for C-Style data definitions and type synonyms

--- test/Malgo/Parser/CStyleSpec.hs
+++ test/Malgo/Parser/CStyleSpec.hs
@@ -1,12 +1,9 @@
 module Malgo.Parser.CStyleSpec (spec) where
 
-import Data.ByteString.Lazy qualified as BL
 import Malgo.Monad (runMalgoM)
 import Malgo.Parser.CStyle (parseCStyle)
 import Malgo.Prelude
-import Malgo.SExpr qualified as SExpr
 import Malgo.TestUtils
-import System.FilePath ((</>))
 import Test.Hspec
 import Text.Megaparsec (errorBundlePretty)
 
@@ -51,30 +48,3 @@ spec = describe "C-Style Parser" do
     case result of
       Left err -> expectationFailure $ errorBundlePretty err
       Right _ -> pure ()
-
-  -- Golden tests for C-style data definitions and type synonyms
-  golden "CStyleDataDef" (driveCStyleParse (testcaseDir </> "CStyleDataDef.mlg"))
-  golden "CStyleDataDef sexpr" (driveCStyleParseSExpr (testcaseDir </> "CStyleDataDef.mlg"))
-  golden "CStyleTypeSynonym" (driveCStyleParse (testcaseDir </> "CStyleTypeSynonym.mlg"))
-  golden "CStyleTypeSynonym sexpr" (driveCStyleParseSExpr (testcaseDir </> "CStyleTypeSynonym.mlg"))
-
--- Drive functions for C-style parsing golden tests
-driveCStyleParse :: FilePath -> IO String
-driveCStyleParse srcPath = do
-  src <- convertString <$> BL.readFile srcPath
-  runMalgoM flag do
-    parsed <- parseCStyle srcPath src
-    case parsed of
-      Left err -> error $ errorBundlePretty err
-      Right parsed ->
-        pure $ pShowCompact parsed
-
-driveCStyleParseSExpr :: FilePath -> IO String
-driveCStyleParseSExpr srcPath = do
-  src <- convertString <$> BL.readFile srcPath
-  runMalgoM flag do
-    parsed <- parseCStyle srcPath src
-    case parsed of
-      Left err -> error $ errorBundlePretty err
-      Right parsed ->
-        pure $ pShowCompact $ SExpr.toSExpr parsed
